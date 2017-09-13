e0c1f532a2d389460cff3ca6e64c7cd29196b7f4
nastyamyalik <nastia.15@inbox.ru>
2017-09-13 19:27:28 +0300

    more tasks

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -161,7 +161,7 @@ function getStringsLength(arr) {
  *    [ 1, 'b', 'c'], 0, 'x'  => [ 'x', 1, 'b', 'c' ]
  */
 function insertItem(arr, item, index) {
-   throw new Error('Not implemented');
+    return arr.splice(index, 0,item);
 }
 
 /**
@@ -175,7 +175,7 @@ function insertItem(arr, item, index) {
  *    [ 'a', 'b', 'c', 'd'], 3  => [ 'a', 'b', 'c' ]
  */
 function getHead(arr, n) {
-   throw new Error('Not implemented');
+   return arr.slice(0,n);
 }
 
 
@@ -190,7 +190,8 @@ function getHead(arr, n) {
  *    [ 'a', 'b', 'c', 'd'], 3  => [ 'b', 'c', 'd' ]
  */
 function getTail(arr, n) {
-   throw new Error('Not implemented');
+    var arr2 = arr.slice(-n);
+    return arr2;
 }
 
 
@@ -215,7 +216,7 @@ function getTail(arr, n) {
  *    +'30,31,32,33,34'
  */
 function toCsvText(arr) {
-   throw new Error('Not implemented');
+   return arr.map((x) => (x.join(','))).join('\n');
 }
 
 /**
@@ -230,7 +231,7 @@ function toCsvText(arr) {
  *   [ 10, 100, -1 ]      => [ 100, 10000, 1 ]
  */
 function toArrayOfSquares(arr) {
-   throw new Error('Not implemented');
+    return arr.map((num) => (num*num));
 }
 
 
@@ -249,7 +250,7 @@ function toArrayOfSquares(arr) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
 function getMovingSum(arr) {
-   throw new Error('Not implemented');
+   return arr.map((x,i)=>(arr.slice(0, i+1)).reduce((a,b)=>(a+b)));
 }
 
 /**
