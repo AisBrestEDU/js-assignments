19c9bbe74ea927177b3d99e404baddaad8397b9d
Sergey Dubovich <sergey.dubovich@aisnovations.com>
2017-09-14 17:03:57 +0300

    + task 3

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -22,7 +22,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   throw new Error('Not implemented');
+   return new Date(value);
 }
 
 /**
@@ -37,7 +37,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   throw new Error('Not implemented');
+    return Date.parse(value);
 }
 
 
@@ -56,7 +56,10 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+   if ( date.getFullYear() % 4 )     return false;
+   if ( date.getFullYear() % 100 )   return true;
+   if ( date.getFullYear() % 400 )   return false;
+   return true;
 }
 
 
@@ -76,7 +79,11 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-   throw new Error('Not implemented');
+    let Timespan = new Date( endDate.valueOf() - startDate.valueOf());
+    let fullHours = Math.floor( Timespan.valueOf() / ( 60 * 60 * 1000 ) ); // minutes * seconds * milliseconds
+    if ( fullHours < 10 && fullHours >= 0 )
+        fullHours = "0" + fullHours;
+    return `${fullHours}${Timespan.toISOString().slice(13,23)}`;
 }
 
 
@@ -94,7 +101,11 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-    throw new Error('Not implemented');
+    let hourAngle = 0.5 * (60 * (date.getUTCHours() % 12) + date.getUTCMinutes());
+    let minuteAngle = 6 * date.getUTCMinutes();
+    let degreeAngle = Math.abs( hourAngle - minuteAngle);
+    if(degreeAngle > 180)   degreeAngle %= 180;
+    return degreeAngle * Math.PI / 180;  
 }
 
 
