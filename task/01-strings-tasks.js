d5c286ad5a8767a7466ffdb83428979fe2228e1e
aleksey <AWork129@yandex.by>
2017-09-16 21:44:22 +0300

    getFirstChar

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -87,7 +87,8 @@ function extractNameFromTemplate(value) {
  *   'cat'       => 'c'
  */
 function getFirstChar(value) {
-    throw new Error('Not implemented');
+    //throw new Error('Not implemented');
+    return value[0];
 }
 
 /**
