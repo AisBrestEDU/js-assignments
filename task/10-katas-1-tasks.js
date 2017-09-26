92264b00363a8658240057fc290b3e9ee529746c
maksim.sarvas <ladamashina@gmail.com>
2017-09-26 19:17:22 +0300

    10  1 of 4 done

--- task/10-katas-1-tasks.js
+++ task/10-katas-1-tasks.js
@@ -88,8 +88,29 @@ function* expandBraces(str) {
  *
  */
 function getZigZagMatrix(n) {
-    throw new Error('Not implemented');
+
+  
+        
+        let mtx = [];
+        for (let i = 0; i < n; i++) 
+           mtx[i] = [];
+     
+        let i=1, j=1;
+        for (let e = 0; e < n*n; e++) {
+            mtx[i-1][j-1] = e;
+            if ((i + j) % 2 == 0) {
+                if (j < n) j ++;
+                else       i += 2;
+                if (i > 1) i --;
+            } else {
+                if (i < n) i ++;
+                else       j += 2;
+                if (j > 1) j --;
+            }
+        }  
+    return    mtx;
 }
+
 
 
 /**
@@ -113,7 +134,21 @@ function getZigZagMatrix(n) {
  *
  */
 function canDominoesMakeRow(dominoes) {
-    throw new Error('Not implemented');
+    let counter = 0;
+   for(let i = 1;i<dominoes.length;i++){
+       for(let j=0;j<=1;j++){
+
+        if(dominoes[i][j]!==dominoes[i-1][j]&&dominoes[i][j]!==dominoes[i-1][j+1]&&dominoes[i][j]!==dominoes[i-1][j-1]){
+            counter++;
+        }
+        else {}
+   }
+   if(counter == 2 ){
+       return false;
+   } 
+   else counter = 0;
+}
+    return true;
 }
 
 
