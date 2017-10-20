482de0538946d7d2f20732d24f1212042909566e
alesha <work04092017@gmail.com>
2017-10-20 11:09:12 +0300

    getMovingSum

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -262,7 +262,9 @@ function toArrayOfSquares(arr) {
  *   [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ] => [ 1, 3, 6, 10, 15, 21, 28, 36, 45, 55 ]
  */
 function getMovingSum(arr) {
-   throw new Error('Not implemented');
+   //throw new Error('Not implemented');
+
+   return arr.map((item,n,array)=>item+array.reduce((acc,value,indx)=>(indx<n)?acc+value:acc,0));
 }
 
 /**
