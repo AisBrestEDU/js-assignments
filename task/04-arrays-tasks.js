0fd830eac10510335bc99ea902775c302082f9ea
alesha <work04092017@gmail.com>
2017-10-20 10:39:36 +0300

    insertItem

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -161,7 +161,11 @@ function getStringsLength(arr) {
  *    [ 1, 'b', 'c'], 0, 'x'  => [ 'x', 1, 'b', 'c' ]
  */
 function insertItem(arr, item, index) {
-   throw new Error('Not implemented');
+   //throw new Error('Not implemented');
+
+   arr.splice(index,0,item);
+
+   return arr;
 
 }
 
