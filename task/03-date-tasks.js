ba508bff44df8f65f0bdd45bee8e7a6836d5c0e9
Orlenko <enerGOww@gmail.com>
2020-06-30 19:39:19 +0300

    3 task complete

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -22,7 +22,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   throw new Error('Not implemented');
+   return new Date(value)
 }
 
 /**
@@ -37,7 +37,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   throw new Error('Not implemented');
+   return new Date(value)
 }
 
 
@@ -56,7 +56,15 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+   const year = date.getFullYear()
+   if (year % 4 !== 0) {
+     return false
+   } else if (year % 100 !== 0) {
+     return true
+   } else if (year % 400 !== 0) {
+     return false
+   }
+   return true
 }
 
 
@@ -76,14 +84,20 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-   throw new Error('Not implemented');
+  const diff = endDate - startDate
+  const date = new Date(endDate - startDate)
+  const hours = ('00' + Math.floor(diff / (3600 * 1000))).slice(-2)
+  const minutes = ('00' + date.getMinutes()).slice(-2)
+  const seconds = ('00' + date.getSeconds()).slice(-2)
+  const milliSeconds = ('000' + date.getMilliseconds()).slice(-3)
+  return `${hours}:${minutes}:${seconds}.${milliSeconds}`
 }
 
 
 /**
  * Returns the angle (in radians) between the hands of an analog clock for the specified Greenwich time.
  * If you have problem with solution please read: https://en.wikipedia.org/wiki/Clock_angle_problem
- * 
+ *
  * @param {date} date
  * @return {number}
  *
@@ -94,7 +108,17 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-    throw new Error('Not implemented');
+  const hours = date.getUTCHours() % 12
+  const minutes = date.getUTCMinutes()
+
+  let angle = Math.abs((60 * hours - 11 * minutes) / 2)
+
+  if (angle > 180) {
+    angle = 360 - angle
+  }
+
+  angle = (angle * Math.PI) / 180
+  return angle
 }
 
 
