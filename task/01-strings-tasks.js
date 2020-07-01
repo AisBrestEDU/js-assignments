123a95dbfcf01c0e99e63e54484e492f8c99ab9a
Кирилл Курко <kirillkurko2609@gmail.com>
2020-07-01 20:08:01 +0300

    extractEmails solution added.

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -178,7 +178,8 @@ function convertToUpperCase(str) {
  *   'info@gmail.com' => ['info@gmail.com']
  */
 function extractEmails(str) {
-    throw new Error('Not implemented');
+    return str.split(';');
+    //throw new Error('Not implemented');
 }
 
 /**
