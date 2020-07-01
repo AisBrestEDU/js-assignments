8461cacd4fffa385d2d301bbc6660147950769bd
Кирилл Курко <kirillkurko2609@gmail.com>
2020-07-01 19:34:14 +0300

    removeLeadingAndTrailingWhitespaces solution added.

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -99,7 +99,8 @@ function getFirstChar(value) {
  *   '\tHello, World! ' => 'Hello, World!'
  */
 function removeLeadingAndTrailingWhitespaces(value) {
-    throw new Error('Not implemented');
+    return value.trim();
+   // throw new Error('Not implemented');
 }
 
 /**
