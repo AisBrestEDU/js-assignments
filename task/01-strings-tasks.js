eaf2b5d70ffac53d518c6ecafc1f54158facb639
Кирилл Курко <kirillkurko2609@gmail.com>
2020-07-01 19:32:29 +0300

    getFirstChar solution added.

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -70,7 +70,6 @@ function getStringFromTemplate(firstName, lastName) {
  */
 function extractNameFromTemplate(value) {
     return value.substr(7, value.length - 8);
-    throw new Error('Not implemented');
 }
 
 
@@ -85,7 +84,7 @@ function extractNameFromTemplate(value) {
  *   'cat'       => 'c'
  */
 function getFirstChar(value) {
-    throw new Error('Not implemented');
+    return value.charAt(0);
 }
 
 /**
