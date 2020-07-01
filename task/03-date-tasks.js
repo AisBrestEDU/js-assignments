e63fa371bd621cac675e3cda6290f0071be0699a
Aleksan4e3 <aleksan4e3@gmail.com>
2020-07-01 13:22:53 +0300

    Completed task3

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -22,7 +22,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   throw new Error('Not implemented');
+   return new Date(value);
 }
 
 /**
@@ -37,7 +37,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   throw new Error('Not implemented');
+   return new Date(value);
 }
 
 
@@ -56,7 +56,12 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+   let year = date.getFullYear();
+
+   if (year % 4 != 0) return false;
+   else if (year % 100 != 0) return true;
+   else if (year % 400 != 0) return false;
+   else return true;
 }
 
 
@@ -76,7 +81,18 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-   throw new Error('Not implemented');
+   let pad = number => number < 10 ? '0' + number : number;
+
+   let milliSeconds = endDate - startDate;
+   let hours = Math.floor(milliSeconds / 3600000);
+   milliSeconds %= 3600000;
+   let minutes = Math.floor(milliSeconds / 60000);
+   milliSeconds %= 60000;
+   let seconds = Math.floor(milliSeconds / 1000);
+   milliSeconds %= 1000;
+   milliSeconds = (milliSeconds / 1000).toFixed(3).slice(2, 5)
+
+   return `${pad(hours)}:${pad(minutes)}:${pad(seconds)}.${milliSeconds}`;
 }
 
 
@@ -94,14 +110,22 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-    throw new Error('Not implemented');
+   let hours = date.getUTCHours();
+   let minutes = date.getUTCMinutes();
+   let hourHandAngle = 0.5 * (60 * hours + minutes);
+   hourHandAngle = hours > 12 ? hourHandAngle - 360 : hourHandAngle;
+   let minuteHandAngle = 6 * minutes;
+   let angle = hourHandAngle - minuteHandAngle;
+   angle = angle > 180 ? angle % 180 : angle;
+   
+   return Math.abs(angle) * Math.PI / 180;
 }
 
 
 module.exports = {
-    parseDataFromRfc2822: parseDataFromRfc2822,
-    parseDataFromIso8601: parseDataFromIso8601,
-    isLeapYear: isLeapYear,
-    timeSpanToString: timeSpanToString,
-    angleBetweenClockHands: angleBetweenClockHands
+   parseDataFromRfc2822: parseDataFromRfc2822,
+   parseDataFromIso8601: parseDataFromIso8601,
+   isLeapYear: isLeapYear,
+   timeSpanToString: timeSpanToString,
+   angleBetweenClockHands: angleBetweenClockHands
 };
