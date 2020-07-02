195adf1c60549bc12aa7dc7d350c7a87ea809afc
20kMemovUnderWater <starovoyt_1995@mail.ru>
2020-07-02 11:03:07 +0300

    fix

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -37,7 +37,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-    throw new Error('Not implemented');
+     throw new Error('Not implemented');
 }
 
 
@@ -56,7 +56,7 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-    throw new Error('Not implemented');
+     throw new Error('Not implemented');
 }
 
 
@@ -76,7 +76,7 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) { 
-    throw new Error('Not implemented');
+     throw new Error('Not implemented');
 }
 
 
@@ -94,7 +94,7 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-    throw new Error('Not implemented');
+     throw new Error('Not implemented');
 }
 
 
