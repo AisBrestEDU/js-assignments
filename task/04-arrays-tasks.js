3b7f147021268ae3f138c56547c8e891ae229413
yauhenikh <khotin.13.nomer@gmail.com>
2020-07-02 12:25:31 +0300

    Fix bug with Task4 (swap head and tail of array)

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -601,8 +601,8 @@ function getElementByIndexes(arr, indexes) {
  */
 function swapHeadAndTail(arr) {
    return arr.slice(Math.ceil(arr.length / 2))
-             .concat(arr.slice(Math.floor(arr.length / 2), Math.ceil(arr.length / 2)))
-             .concat(arr.slice(Math.ceil(arr.length / 2)));
+      .concat(arr.slice(Math.floor(arr.length / 2), Math.ceil(arr.length / 2)))
+      .concat(arr.slice(0, Math.floor(arr.length / 2)));
 }
 
 
