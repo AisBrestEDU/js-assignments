7952b1d3fbdfb89e8f77e0b1e3389f3c4ecb01a1
Anapreichyk Danil <dananaprey@gmail.com>
2020-07-03 00:51:29 +0300

    03-date and 04-arrays dananaprey

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -22,9 +22,9 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   throw new Error('Not implemented');
+   return new Date(value);
 }
-
+//npm test ./test/03-date-tests.js
 /**
  * Parses an ISO 8601 string date representation into date value
  * For ISO 8601 date specification refer to : https://en.wikipedia.org/wiki/ISO_8601
@@ -37,7 +37,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   throw new Error('Not implemented');
+   return new Date(value);
 }
 
 
@@ -56,7 +56,9 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+   const year = date.getFullYear();
+
+   return (year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0);
 }
 
 
@@ -76,7 +78,21 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-   throw new Error('Not implemented');
+   // hours
+   let hours = endDate.getHours() - startDate.getHours();
+   if (startDate.getDay() !== endDate.getDay()) hours = hours + 24;
+   hours = hours < 10 ? "0" + hours : hours;
+   // minutes
+   let minutes = endDate.getMinutes() - startDate.getMinutes();
+   minutes = minutes < 10 ? "0" + minutes : minutes;
+   // seconds
+   let seconds = endDate.getSeconds() - startDate.getSeconds();
+   seconds = seconds < 10 ? "0" + seconds : seconds;
+   // milliseconds
+   let milliseconds = endDate.getMilliseconds() - startDate.getMilliseconds();
+   milliseconds = milliseconds < 100 ? "00" + milliseconds : milliseconds;
+
+   return `${hours}:${minutes}:${seconds}.${milliseconds}`;
 }
 
 
@@ -94,7 +110,9 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-    throw new Error('Not implemented');
+   const angle = 0.5 * (60 * date.getUTCHours() - 11 * date.getUTCMinutes());
+
+   return (angle > 180 ? Math.abs(angle - 360 * Math.round(angle / 360)) : angle) * Math.PI / 180;
 }
 
 
