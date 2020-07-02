889aead23e331618058bb80c410b18d0e9a9e11d
Maria Kiselyova <mariya.kiselyova95@gmail.com>
2020-07-02 21:36:50 +0300

    task3

--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -22,7 +22,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   throw new Error('Not implemented');
+   return new Date(value);
 }
 
 /**
@@ -37,7 +37,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   throw new Error('Not implemented');
+   return new Date(value);
 }
 
 
@@ -56,7 +56,8 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+   let year = date.getFullYear();
+   return ((year % 4 === 0) && (year % 100 !== 0)) || (year % 400 === 0);
 }
 
 
@@ -76,7 +77,40 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-   throw new Error('Not implemented');
+
+   let ms = endDate.getMilliseconds() -  startDate.getMilliseconds();
+   let sec = endDate.getSeconds() -  startDate.getSeconds();
+   let min = endDate.getMinutes() -  startDate.getMinutes();
+   let hour = endDate.getHours() -  startDate.getHours();
+
+
+   let dateDiff = (endDate-startDate)-ms-sec*1000-min*1000*60-hour*1000*60*60;
+   const msInDay=86400000;
+   let days = dateDiff/msInDay;
+   hour=hour+days*24;
+
+   // Before ECMAScript 2017
+
+   // if (hour<10){
+   //    hour="0"+hour;
+   // }
+   // if (min<10){
+   //    min="0"+min;
+   // }
+   // if (sec<10){
+   //    sec="0"+sec;
+   // }
+   // if (ms<10){
+   //    ms="00"+ms;
+   // } else if (ms<100){
+   //    ms="0"+ms;
+   // }
+   // return hour+":"+min+":"+sec+"."+ms;
+
+
+   // ECMAScript 2017
+   return String(hour).padStart(2,'0')+":"+String(min).padStart(2,'0')+":"+String(sec).padStart(2,'0')+"."+String(ms).padStart(3,'0');
+   
 }
 
 
@@ -94,7 +128,12 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-    throw new Error('Not implemented');
+    let hour24 = date.getUTCHours();
+    let hour= (hour24>12)?(hour24-12):(hour24);
+    let minute = date.getUTCMinutes();
+    let degrees=Math.abs(0.5*(60*hour-11*minute));
+    let minAngle=(degrees>180)?(360-degrees):(degrees);
+    return (Math.PI/180)*minAngle;
 }
 
 
