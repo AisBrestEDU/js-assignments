c7d3a794e0769386514bef8d46ade2a46cf51bfc
Dmitriy Astapchik <dim_ast@mail.ru>
2020-07-03 22:59:05 +0300

    Fixed failed tests

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -270,11 +270,11 @@ function getSecondItems(arr) {
  *  [ 1,2,3,4,5 ] => [ 1, 2,2, 3,3,3, 4,4,4,4, 5,5,5,5,5 ]
  */
 function propagateItemsByPositionIndex(arr) {
-    return arr.flatMap((v, i) => {
+    return arr.map((v, i) => {
         var a = new Array(i + 1);
         a.fill(v);
         return a;
-    });
+    }).flat();
 }
 
 
@@ -547,7 +547,7 @@ function group(array, keySelector, valueSelector) {
  *   ['one','two','three'], x=>x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
 function selectMany(arr, childrenSelector) {
-    return arr.flatMap(v => childrenSelector(v));
+    return arr.map(v => childrenSelector(v)).flat();
 }
 
 
