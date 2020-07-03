9bed31a06340ed6abcb7018f8dccd737da5037a7
marioti95 <dkmarioti27@gmail.com>
2020-07-03 12:21:40 +0200

    Revert "Revert "Revert "#1-4Task"""
    This reverts commit ea3e95a92b97b2b7ff7e207cde681dd4d00f649b.

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -9,6 +9,7 @@
  *                                                                                          *
  ********************************************************************************************/
 
+
 /**
  * Returns an area of a rectangle given by width and heigth.
  *
@@ -21,8 +22,9 @@
  *   5, 5  => 25
  */
 function getRectangleArea(width, height) {
-  return width * height;
+    throw new Error('Not implemented');
 }
+
 
 /**
  * Returns a circumference of circle given by radius.
@@ -36,7 +38,7 @@ function getRectangleArea(width, height) {
  *   0    => 0
  */
 function getCicleCircumference(radius) {
-  return 2 * Math.PI * radius;
+    throw new Error('Not implemented');
 }
 
 /**
@@ -52,7 +54,7 @@ function getCicleCircumference(radius) {
  *  -3, 3  => 0
  */
 function getAverage(value1, value2) {
-  return value1 / 2 + value2 / 2;
+    throw new Error('Not implemented');
 }
 
 /**
@@ -71,7 +73,7 @@ function getAverage(value1, value2) {
  *   (-5,0) (10,-10) => 18.027756377319946
  */
 function getDistanceBetweenPoints(x1, y1, x2, y2) {
-  return Math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2);
+    throw new Error('Not implemented');
 }
 
 /**
@@ -87,8 +89,9 @@ function getDistanceBetweenPoints(x1, y1, x2, y2) {
  *   5*x = 0         => 0
  */
 function getLinearEquationRoot(a, b) {
-  return b / -a;
+    throw new Error('Not implemented');
 }
+
 
 /**
  * Returns an angle (in radians) between two vectors given by xi and yi, coordinates in Cartesian plane
@@ -108,10 +111,7 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (1,2)     => 0
  */
 function getAngleBetweenVectors(x1, y1, x2, y2) {
-  return Math.acos(
-    (x1 * x2 + y1 * y2) /
-      (Math.sqrt(x1 ** 2 + y1 ** 2) * Math.sqrt(x2 ** 2 + y2 ** 2))
-  );
+    throw new Error('Not implemented');
 }
 
 /**
@@ -127,8 +127,9 @@ function getAngleBetweenVectors(x1, y1, x2, y2) {
  *     0     => 0
  */
 function getLastDigit(value) {
-  return value % 10;
+    throw new Error('Not implemented');
 }
+
 
 /**
  * Returns a number by given string representation.
@@ -142,7 +143,7 @@ function getLastDigit(value) {
  * '-525.5'     => -525.5
  */
 function parseNumberFromString(value) {
-  return +value;
+    throw new Error('Not implemented');
 }
 
 /**
@@ -158,8 +159,8 @@ function parseNumberFromString(value) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelipidedDiagonal(a, b, c) {
-  return Math.sqrt(a ** 2 + b ** 2 + c ** 2);
+function getParallelipidedDiagonal(a,b,c) {
+    throw new Error('Not implemented');
 }
 
 /**
@@ -168,7 +169,7 @@ function getParallelipidedDiagonal(a, b, c) {
  * @param {number} num
  * @param {number} pow
  * @return {number}
- *
+ *  
  * @example:
  *   1234, 0  => 1234
  *   1234, 1  => 1230
@@ -180,7 +181,7 @@ function getParallelipidedDiagonal(a, b, c) {
  *   1678, 3  => 2000
  */
 function roundToPowerOfTen(num, pow) {
-  return Math.round(num / 10 ** pow) * 10 ** pow;
+    throw new Error('Not implemented');
 }
 
 /**
@@ -189,7 +190,7 @@ function roundToPowerOfTen(num, pow) {
  *
  * @param {number} n
  * @return {bool}
- *
+ * 
  * @example:
  *   4 => false
  *   5 => true
@@ -201,10 +202,7 @@ function roundToPowerOfTen(num, pow) {
  *   17 => true
  */
 function isPrime(n) {
-  for (let i = 2; i <= Math.sqrt(n); i++) {
-    if (n % i === 0) return false;
-  }
-  return n > 1;
+    throw new Error('Not implemented');
 }
 
 /**
@@ -223,20 +221,20 @@ function isPrime(n) {
  *   toNumber(new Number(42), 0) => 42
  */
 function toNumber(value, def) {
-  return parseInt(value, 10) ? parseInt(value, 10) : def;
+    throw new Error('Not implemented');
 }
 
 module.exports = {
-  getRectangleArea: getRectangleArea,
-  getCicleCircumference: getCicleCircumference,
-  getAverage: getAverage,
-  getDistanceBetweenPoints: getDistanceBetweenPoints,
-  getLinearEquationRoot: getLinearEquationRoot,
-  getAngleBetweenVectors: getAngleBetweenVectors,
-  getLastDigit: getLastDigit,
-  parseNumberFromString: parseNumberFromString,
-  getParallelipidedDiagonal: getParallelipidedDiagonal,
-  roundToPowerOfTen: roundToPowerOfTen,
-  isPrime: isPrime,
-  toNumber: toNumber,
+    getRectangleArea: getRectangleArea,
+    getCicleCircumference: getCicleCircumference,
+    getAverage: getAverage,
+    getDistanceBetweenPoints: getDistanceBetweenPoints,
+    getLinearEquationRoot: getLinearEquationRoot,
+    getAngleBetweenVectors: getAngleBetweenVectors,
+    getLastDigit: getLastDigit,
+    parseNumberFromString: parseNumberFromString,
+    getParallelipidedDiagonal: getParallelipidedDiagonal,
+    roundToPowerOfTen: roundToPowerOfTen,
+    isPrime: isPrime,
+    toNumber: toNumber
 };
