af2e4e1bfa7da4ab2534bc71efa6cde88f1b6a95
Кирилл Курко <kirillkurko2609@gmail.com>
2020-07-03 16:50:40 +0300

    roundToPowerOfTen solution added.

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -184,7 +184,8 @@ function getParallelipidedDiagonal(a,b,c) {
  *   1678, 3  => 2000
  */
 function roundToPowerOfTen(num, pow) {
-    throw new Error('Not implemented');
+    var currentPower = Math.pow(10, pow);
+    return Math.round(num / currentPower) * currentPower; 
 }
 
 /**
