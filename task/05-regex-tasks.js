b8836b49c8d4e189112dbf8c9874ef5054a5038a
Dmitriy Astapchik <dim_ast@mail.ru>
2020-07-04 13:33:51 +0300

    05-regex-tasks.js

--- task/05-regex-tasks.js
+++ task/05-regex-tasks.js
@@ -31,7 +31,8 @@
  * @return {RegExp}
  */
 function getRegexForGuid() {
-   throw new Error('Not implemented');
+    var pt = '[0-9a-f]';
+    return new RegExp(`{${pt}{8}-${pt}{4}-${pt}{4}-${pt}{4}-${pt}{12}}`, 'i');
 }
 
 
@@ -53,7 +54,7 @@ function getRegexForGuid() {
  *
  */
 function getRegexForPitSpot() {
-   throw new Error('Not implemented');
+    return /[is]/;
 }
 
 
@@ -72,7 +73,7 @@ function getRegexForPitSpot() {
  * @return {RegExp}
  */
 function getRegexForIPv4() {
-   throw new Error('Not implemented');
+    return /^((25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
 }
 
 
@@ -91,7 +92,7 @@ function getRegexForIPv4() {
  * @return {RegExp}
  */
 function getRegexForSSN() {
-   throw new Error('Not implemented');
+    return /(?!000)\d{3}-(?!00)\d{2}-(?!0000)\d{4}/;
 }
 
 
@@ -116,7 +117,7 @@ function getRegexForSSN() {
  *   'Pa55'.match(validator) => false
  */
 function getPasswordValidator(minLength) {
-   throw new Error('Not implemented');
+    return RegExp(`^(?=[0-9a-zA-Z]{${minLength},}$)(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])`);
 }
 
 
