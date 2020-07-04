d8b948feb26f4ced671cca999f306f54f1543315
Oleksandr Kovalenko <alex.trapper@gmail.com>
2020-07-04 21:55:26 +0300

    Numbers tasks done

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -21,9 +21,7 @@
  *   5, 10 => 50
  *   5, 5  => 25
  */
-function getRectangleArea(width, height) {
-    throw new Error('Not implemented');
-}
+let getRectangleArea = (width, height) => width * height;
 
 
 /**
@@ -37,9 +35,7 @@ function getRectangleArea(width, height) {
  *   3.14 => 19.729201864543903
  *   0    => 0
  */
-function getCicleCircumference(radius) {
-    throw new Error('Not implemented');
-}
+let getCicleCircumference = (radius) => 2 * Math.PI * radius;
 
 /**
  * Returns an average of two given numbers.
@@ -53,9 +49,7 @@ function getCicleCircumference(radius) {
  *  10, 0  => 5
  *  -3, 3  => 0
  */
-function getAverage(value1, value2) {
-    throw new Error('Not implemented');
-}
+let getAverage = (value1, value2) => 0.5 * value1 + 0.5 * value2;
 
 /**
  * Returns a distance beetween two points by cartesian coordinates.
@@ -72,9 +66,7 @@ function getAverage(value1, value2) {
  *   (0,0) (1,0)    => 1
  *   (-5,0) (10,-10) => 18.027756377319946
  */
-function getDistanceBetweenPoints(x1, y1, x2, y2) {
-    throw new Error('Not implemented');
-}
+let getDistanceBetweenPoints = (x1, y1, x2, y2) => Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
 
 /**
  * Returns a root of linear equation a*x + b = 0 given by coefficients a and b.
@@ -88,9 +80,7 @@ function getDistanceBetweenPoints(x1, y1, x2, y2) {
  *   x + 8 = 0       => -8
  *   5*x = 0         => 0
  */
-function getLinearEquationRoot(a, b) {
-    throw new Error('Not implemented');
-}
+let getLinearEquationRoot = (a, b) => - (b / a);
 
 
 /**
@@ -110,9 +100,8 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (0,1)     => 0
  *   (0,1) (1,2)     => 0
  */
-function getAngleBetweenVectors(x1, y1, x2, y2) {
-    throw new Error('Not implemented');
-}
+let getAngleBetweenVectors = (x1, y1, x2, y2) =>
+    Math.acos((x1 * x2 + y1 * y2 )/ (Math.sqrt(Math.pow(x1, 2) + Math.pow(y1, 2)) * Math.sqrt(Math.pow(x2, 2) + Math.pow(y2, 2))));
 
 /**
  * Returns a last digit of a integer number.
@@ -127,7 +116,8 @@ function getAngleBetweenVectors(x1, y1, x2, y2) {
  *     0     => 0
  */
 function getLastDigit(value) {
-    throw new Error('Not implemented');
+    let strRepresentation = '' + value;
+    return strRepresentation.substr(strRepresentation.length - 1);
 }
 
 
@@ -142,9 +132,7 @@ function getLastDigit(value) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(value) {
-    throw new Error('Not implemented');
-}
+let parseNumberFromString = (value) => + value;
 
 /**
  * Returns a diagonal length of the rectangular parallelepiped given by its sides a,b,c.
@@ -159,9 +147,8 @@ function parseNumberFromString(value) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelipidedDiagonal(a,b,c) {
-    throw new Error('Not implemented');
-}
+let getParallelipidedDiagonal = (a, b, c) =>
+    Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2));
 
 /**
  * Returns the number rounded to specified power of 10.
@@ -181,7 +168,8 @@ function getParallelipidedDiagonal(a,b,c) {
  *   1678, 3  => 2000
  */
 function roundToPowerOfTen(num, pow) {
-    throw new Error('Not implemented');
+    let precision = +('1' + '0'.repeat(pow));
+    return Math.round(num / precision, 0) * precision;
 }
 
 /**
@@ -202,7 +190,12 @@ function roundToPowerOfTen(num, pow) {
  *   17 => true
  */
 function isPrime(n) {
-    throw new Error('Not implemented');
+    for (let i = 2; i <= Math.sqrt(n); i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+    return true;
 }
 
 /**
@@ -220,9 +213,7 @@ function isPrime(n) {
  *   toNumber(42, 0) => 42
  *   toNumber(new Number(42), 0) => 42
  */
-function toNumber(value, def) {
-    throw new Error('Not implemented');
-}
+let toNumber = (value, def) => !isNaN(value) && !isNaN(parseFloat(value)) ? +value : def;
 
 module.exports = {
     getRectangleArea: getRectangleArea,
