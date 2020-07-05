769cd2c2758ace2f7b0192200c9c45accf785b50
Ilya Leanovich <leanovich.ilya@gmail.com>
2020-07-05 17:45:11 +0300

    fix: update task 1

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -328,13 +328,13 @@ function isString(value) {
  *   'K♠' => 51
  */
 function getCardId(value) {
-    let array =
+    let arrayCards =
       ['A♣', '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣',
           'A♦', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦',
           'A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥',
           'A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠'];
 
-    return array.indexOf(value);
+    return arrayCards.indexOf(value);
 }
 
 
