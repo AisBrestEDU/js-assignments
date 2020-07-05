848476ceee1023aab4aa9d0d01e94b85aac17fe2
Maksim Litvyakov <litvy-maksim@yandex.ru>
2020-07-05 12:30:49 +0700

    getLinearEquationRoot done

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -89,7 +89,8 @@ function getDistanceBetweenPoints(x1, y1, x2, y2) {
  *   5*x = 0         => 0
  */
 function getLinearEquationRoot(a, b) {
-    throw new Error('Not implemented');
+    let x = (-b) / a;
+    return x;
 }
 
 
