ffc485dc235bfae70c39ebfae362e06a915a536a
Maksim Litvyakov <litvy-maksim@yandex.ru>
2020-07-05 13:26:34 +0700

    getParallelipipedDiagonal done

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -161,7 +161,8 @@ function parseNumberFromString(value) {
  *   1,2,3   => 3.741657386773941
  */
 function getParallelipidedDiagonal(a, b, c) {
-    throw new Error('Not implemented');
+    const d = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2));
+    return d;
 }
 
 /**
