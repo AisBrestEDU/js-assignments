fc3edabe5b13a2d646c882392072abb9fca40ce3
Ivan <ivan.lozovskij1@gmail.com>
2020-07-05 12:36:49 +0300

    task_4 v1.01

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -260,6 +260,13 @@ function getSecondItems(arr) {
    return arr.filter((item, index) => index % 2 != 0);
 }
 
+Object.defineProperty(Array.prototype, 'flat', {
+   value: function(depth = 1) {
+     return this.reduce(function (flat, toFlatten) {
+       return flat.concat((Array.isArray(toFlatten) && (depth>1)) ? toFlatten.flat(depth-1) : toFlatten);
+     }, []);
+   }
+});
 
 /**
  * Propagates every item in sequence its position times
