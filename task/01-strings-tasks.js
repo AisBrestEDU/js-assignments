72991fb8d5acfffb628b80b341c2d94866e60a7a
Egor Golubev <egorgolubev12@gmail.com>
2020-07-07 14:17:54 +0300

    1-4 tasks

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -8,7 +8,6 @@
  ********************************************************************************************/
 
 
-
 /**
  * Returns the result of concatenation of two strings.
  *
@@ -22,7 +21,7 @@
  *   '',  'bb'  => 'bb'
  */
 function concatenateStrings(value1, value2) {
-    throw new Error('Not implemented');
+    return value1 + value2;
 }
 
 
@@ -38,7 +37,7 @@ function concatenateStrings(value1, value2) {
  *   ''      => 0
  */
 function getStringLength(value) {
-    throw new Error('Not implemented');
+    return value.length;
 }
 
 /**
@@ -55,7 +54,7 @@ function getStringLength(value) {
  *   'Chuck','Norris'  => 'Hello, Chuck Norris!'
  */
 function getStringFromTemplate(firstName, lastName) {
-    throw new Error('Not implemented');
+    return `Hello, ${firstName} ${lastName}!`;
 }
 
 /**
@@ -69,7 +68,7 @@ function getStringFromTemplate(firstName, lastName) {
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
 function extractNameFromTemplate(value) {
-    throw new Error('Not implemented');
+    return value.slice(7, value.length - 1);
 }
 
 
@@ -84,7 +83,7 @@ function extractNameFromTemplate(value) {
  *   'cat'       => 'c'
  */
 function getFirstChar(value) {
-    throw new Error('Not implemented');
+    return value.charAt(0);
 }
 
 /**
@@ -99,7 +98,7 @@ function getFirstChar(value) {
  *   '\tHello, World! ' => 'Hello, World!'
  */
 function removeLeadingAndTrailingWhitespaces(value) {
-    throw new Error('Not implemented');
+    return value.trim();
 }
 
 /**
@@ -114,12 +113,12 @@ function removeLeadingAndTrailingWhitespaces(value) {
  *   'cat', 3 => 'catcatcat'
  */
 function repeatString(value, count) {
-    throw new Error('Not implemented');
+    return value.repeat(+count);
 }
 
 /**
  * Remove the first occurrence of string inside another string
- * 
+ *
  * @param {string} str
  * @param {string} value
  * @return {string}
@@ -130,7 +129,7 @@ function repeatString(value, count) {
  *   'ABABAB','BA' => 'ABAB'
  */
 function removeFirstOccurrences(str, value) {
-    throw new Error('Not implemented');
+    return str.replace(value, '');
 }
 
 /**
@@ -145,7 +144,7 @@ function removeFirstOccurrences(str, value) {
  *   '<a>' => 'a'
  */
 function unbracketTag(str) {
-    throw new Error('Not implemented');
+    return str.slice(1, str.length - 1);
 }
 
 
@@ -160,7 +159,7 @@ function unbracketTag(str) {
  *  'abcdefghijklmnopqrstuvwxyz' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
  */
 function convertToUpperCase(str) {
-    throw new Error('Not implemented');
+    return str.toUpperCase();
 }
 
 /**
@@ -174,7 +173,7 @@ function convertToUpperCase(str) {
  *   'info@gmail.com' => ['info@gmail.com']
  */
 function extractEmails(str) {
-    throw new Error('Not implemented');
+    return str.split(';');
 }
 
 /**
@@ -201,7 +200,7 @@ function extractEmails(str) {
  *
  */
 function getRectangleString(width, height) {
-    throw new Error('Not implemented');
+    return '┌' + '─'.repeat(width - 2) + '┐' + '\n' + ('│' + ' '.repeat(width - 2) + '│' + '\n').repeat(height - 2) + '└' + '─'.repeat(width - 2) + '┘' + '\n';
 }
 
 
@@ -221,7 +220,18 @@ function getRectangleString(width, height) {
  *
  */
 function encodeToRot13(str) {
-    throw new Error('Not implemented');
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    const rotateAlphabet = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
+    let encodedStr = '';
+
+    for (let j = 0; j < str.length; j++) {
+        if (alphabet.indexOf(str.charAt(j)) !== -1) {
+            encodedStr += rotateAlphabet.charAt(alphabet.indexOf(str.charAt(j)));
+        } else {
+            encodedStr += str.charAt(j);
+        }
+    }
+    return encodedStr;
 }
 
 /**
@@ -238,38 +248,56 @@ function encodeToRot13(str) {
  *   isString(new String('test')) => true
  */
 function isString(value) {
-    throw new Error('Not implemented');
+    return typeof value === 'string' || value instanceof String;
 }
 
 
 /**
  * Returns playid card id.
- * 
+ *
  * Playing cards inittial deck inclides the cards in the following order:
- * 
+ *
  *  'A♣','2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣',
  *  'A♦','2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦',
  *  'A♥','2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥',
  *  'A♠','2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠'
- * 
+ *
  * (see https://en.wikipedia.org/wiki/Standard_52-card_deck)
  * Function returns the zero-based index of specified card in the initial deck above.
- * 
+ *
  * @param {string} value
  * @return {number}
  *
  * @example
  *   'A♣' => 0
- *   '2♣' => 1 
+ *   '2♣' => 1
  *   '3♣' => 2
  *     ...
  *   'Q♠' => 50
  *   'K♠' => 51
  */
 function getCardId(value) {
-    throw new Error('Not implemented');
+    const valueCard = 'A,2,3,4,5,6,7,8,9,10,J,Q,K';
+    const suit = '♣♦♥♠';
+    let cardId;
+    switch (value.length === 3 ? value.charAt(2) : value.charAt(1)) {
+        case '♣':
+            cardId = valueCard.split(',').indexOf(value.charAt(0)) === -1 ? 9 : valueCard.split(',').indexOf(value.charAt(0));
+            break;
+        case '♦':
+            cardId = valueCard.split(',').indexOf(value.charAt(0)) === -1 ? 22 : valueCard.split(',').indexOf(value.charAt(0)) + 13;
+            break;
+        case '♥':
+            cardId = valueCard.split(',').indexOf(value.charAt(0)) === -1 ? 35 : valueCard.split(',').indexOf(value.charAt(0)) + 26;
+            break;
+        case '♠':
+            cardId = valueCard.split(',').indexOf(value.charAt(0)) === -1 ? 48 : valueCard.split(',').indexOf(value.charAt(0)) + 39;
+            break;
+        default:
+            console.log('Wrong value!');
+    }
+    return cardId;
 }
-
 
 module.exports = {
     concatenateStrings: concatenateStrings,
--- task/03-date-tasks.js
+++ task/03-date-tasks.js
@@ -1,3 +1,4 @@
+debugger
 'use strict';
 
 /********************************************************************************************
@@ -22,7 +23,7 @@
  *    'Sun, 17 May 1998 03:00:00 GMT+01' => Date()
  */
 function parseDataFromRfc2822(value) {
-   throw new Error('Not implemented');
+    return new Date(value);
 }
 
 /**
@@ -37,7 +38,7 @@ function parseDataFromRfc2822(value) {
  *    '2016-01-19T08:07:37Z' => Date()
  */
 function parseDataFromIso8601(value) {
-   throw new Error('Not implemented');
+    return Date.parse(value);
 }
 
 
@@ -56,7 +57,8 @@ function parseDataFromIso8601(value) {
  *    Date(2015,1,1)    => false
  */
 function isLeapYear(date) {
-   throw new Error('Not implemented');
+    let year = new Date(date).getFullYear();
+    return !((year % 4) || (!(year % 100) && (year % 400)));
 }
 
 
@@ -76,14 +78,15 @@ function isLeapYear(date) {
  *    Date(2000,1,1,10,0,0),  Date(2000,1,1,15,20,10,453)   => "05:20:10.453"
  */
 function timeSpanToString(startDate, endDate) {
-   throw new Error('Not implemented');
+    let time = new Date(endDate - startDate);
+    return `${parseInt((endDate.getTime() - startDate.getTime()) / (1000 * 60 * 60)) < 10 ? '0' + parseInt((endDate.getTime() - startDate.getTime()) / (1000 * 60 * 60)) : parseInt((endDate.getTime() - startDate.getTime()) / (1000 * 60 * 60))}:${time.getMinutes() < 10 ? '0' + time.getMinutes() : time.getMinutes()}:${time.getSeconds() < 10 ? '0' + time.getSeconds() : time.getSeconds()}.${time.getMilliseconds() < 10 ? '00' + time.getMilliseconds() : time.getMilliseconds() < 100 ? '0' + time.getMilliseconds() : time.getMilliseconds()}`;
 }
 
 
 /**
  * Returns the angle (in radians) between the hands of an analog clock for the specified Greenwich time.
  * If you have problem with solution please read: https://en.wikipedia.org/wiki/Clock_angle_problem
- * 
+ *
  * @param {date} date
  * @return {number}
  *
@@ -94,7 +97,14 @@ function timeSpanToString(startDate, endDate) {
  *    Date.UTC(2016,3,5,21, 0) => Math.PI/2
  */
 function angleBetweenClockHands(date) {
-    throw new Error('Not implemented');
+    let dateFull = new Date(date);
+    let hours = dateFull.getUTCHours() < 12 ? dateFull.getUTCHours() : dateFull.getUTCHours() - 12,
+        minutes = dateFull.getUTCMinutes(),
+        angleHours = 0.5 * (60 * hours + minutes),
+        angleMinutes = 6 * minutes;
+
+    return (Math.abs(angleHours - angleMinutes) > 180 ? 360 - Math.abs(angleHours - angleMinutes) : Math.abs(angleHours - angleMinutes)) * (Math.PI / 180);
+
 }
 
 
