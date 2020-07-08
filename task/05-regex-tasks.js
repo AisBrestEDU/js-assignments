85f057d33a002f53a803374e521418df8fd964b4
Oleksandr Kovalenko <alex.trapper@gmail.com>
2020-07-08 22:22:59 +0300

    Task #5 (RegExp) done

--- task/05-regex-tasks.js
+++ task/05-regex-tasks.js
@@ -31,7 +31,7 @@
  * @return {RegExp}
  */
 function getRegexForGuid() {
-   throw new Error('Not implemented');
+   return /\{[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}\}/;
 }
 
 
@@ -53,7 +53,7 @@ function getRegexForGuid() {
  *
  */
 function getRegexForPitSpot() {
-   throw new Error('Not implemented');
+   return /\w*p.t\w*/;
 }
 
 
@@ -72,7 +72,7 @@ function getRegexForPitSpot() {
  * @return {RegExp}
  */
 function getRegexForIPv4() {
-   throw new Error('Not implemented');
+   return /^([0-2]?\d?\d\.){3}[0-2]?\d?\d$/;
 }
 
 
@@ -91,7 +91,7 @@ function getRegexForIPv4() {
  * @return {RegExp}
  */
 function getRegexForSSN() {
-   throw new Error('Not implemented');
+   return /(?!666|000|9\d{2})\d{3}-(?!00)\d{2}-(?!0{4})\d{4}/;
 }
 
 
@@ -116,7 +116,7 @@ function getRegexForSSN() {
  *   'Pa55'.match(validator) => false
  */
 function getPasswordValidator(minLength) {
-   throw new Error('Not implemented');
+   return new RegExp(`^(?=.*[0-9])(?=.*[^!@#$%^&*\\s])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{${minLength},}`);
 }
 
 
