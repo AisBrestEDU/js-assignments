b673741726a40ed423e26482d61c2510408da556
2reek <mountagest@gmail.com>
2020-07-08 11:26:16 +0300

    Update 01-strings-tasks.js

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -7,6 +7,8 @@
  *                                                                                          *
  ********************************************************************************************/
 
+
+
 /**
  * Returns the result of concatenation of two strings.
  *
@@ -22,6 +24,7 @@
 function concatenateStrings(value1, value2) {
 	return value1 + value2;
 }
+
 
 /**
  * Returns the length of given string.
@@ -55,6 +58,7 @@ function getStringFromTemplate(firstName, lastName) {
 	return `Hello, ${firstName} ${lastName}!`;
 }
 
+
 /**
  * Extracts a name from template string 'Hello, First_Name Last_Name!'.
  *
@@ -71,6 +75,7 @@ function extractNameFromTemplate(value) {
 	return arrayValue.splice(7).join('');
 }
 
+
 /**
  * Returns a first char of the given string.
  *
@@ -84,6 +89,7 @@ function extractNameFromTemplate(value) {
 function getFirstChar(value) {
 	return value.split('').splice(0, 1);
 }
+
 
 /**
  * Removes a leading and trailing whitespace characters from string.
@@ -100,6 +106,7 @@ function removeLeadingAndTrailingWhitespaces(value) {
 	return value.trim();
 }
 
+
 /**
  * Returns a string that repeated the specified number of times.
  *
@@ -115,9 +122,10 @@ function repeatString(value, count) {
 	return value.repeat(count);
 }
 
+
 /**
  * Remove the first occurrence of string inside another string
- *
+ * 
  * @param {string} str
  * @param {string} value
  * @return {string}
@@ -130,6 +138,7 @@ function repeatString(value, count) {
 function removeFirstOccurrences(str, value) {
 	return str.replace(value, '');
 }
+
 
 /**
  * Remove the first and last angle brackets from tag string
@@ -146,6 +155,7 @@ function unbracketTag(str) {
 	return str.replace(/[<>]/g, '');
 }
 
+
 /**
  * Converts all characters of the specified string into the upper case
  *
@@ -160,6 +170,7 @@ function convertToUpperCase(str) {
 	return str.toUpperCase();
 }
 
+
 /**
  * Extracts e-mails from single string with e-mails list delimeted by semicolons
  *
@@ -173,6 +184,7 @@ function convertToUpperCase(str) {
 function extractEmails(str) {
 	return str.split(';');
 }
+
 
 /**
  * Returns the string representation of rectangle with specified width and height
@@ -216,6 +228,7 @@ function getRectangleString(width, height) {
 	return topLine + midLine.repeat(height - 2) + botLine;
 }
 
+
 /**
  * Encode specified string with ROT13 cipher
  * See details:  https://en.wikipedia.org/wiki/ROT13
@@ -245,6 +258,7 @@ function encodeToRot13(str) {
 	return result;
 }
 
+
 /**
  * Returns true if the value is string; otherwise false.
  * @param {string} value
@@ -262,25 +276,26 @@ function isString(value) {
 	return typeof value === 'string' || value instanceof String;
 }
 
+
 /**
  * Returns playid card id.
- *
+ * 
  * Playing cards inittial deck inclides the cards in the following order:
- *
+ * 
  *  'A♣','2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣',
  *  'A♦','2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦',
  *  'A♥','2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥',
  *  'A♠','2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠'
- *
+ * 
  * (see https://en.wikipedia.org/wiki/Standard_52-card_deck)
  * Function returns the zero-based index of specified card in the initial deck above.
- *
+ * 
  * @param {string} value
  * @return {number}
  *
  * @example
  *   'A♣' => 0
- *   '2♣' => 1
+ *   '2♣' => 1 
  *   '3♣' => 2
  *     ...
  *   'Q♠' => 50
@@ -345,20 +360,21 @@ function getCardId(value) {
 	return cards.indexOf(value);
 }
 
+
 module.exports = {
-	concatenateStrings: concatenateStrings,
-	getStringLength: getStringLength,
-	getStringFromTemplate: getStringFromTemplate,
-	extractNameFromTemplate: extractNameFromTemplate,
-	getFirstChar: getFirstChar,
-	removeLeadingAndTrailingWhitespaces: removeLeadingAndTrailingWhitespaces,
-	repeatString: repeatString,
-	removeFirstOccurrences: removeFirstOccurrences,
-	unbracketTag: unbracketTag,
-	convertToUpperCase: convertToUpperCase,
-	extractEmails: extractEmails,
-	getRectangleString: getRectangleString,
-	encodeToRot13: encodeToRot13,
-	isString: isString,
-	getCardId: getCardId,
+    concatenateStrings: concatenateStrings,
+    getStringLength: getStringLength,
+    getStringFromTemplate: getStringFromTemplate,
+    extractNameFromTemplate: extractNameFromTemplate,
+    getFirstChar: getFirstChar,
+    removeLeadingAndTrailingWhitespaces: removeLeadingAndTrailingWhitespaces,
+    repeatString: repeatString,
+    removeFirstOccurrences: removeFirstOccurrences,
+    unbracketTag: unbracketTag,
+    convertToUpperCase: convertToUpperCase,
+    extractEmails: extractEmails,
+    getRectangleString: getRectangleString,
+    encodeToRot13: encodeToRot13,
+    isString: isString,
+    getCardId: getCardId
 };
