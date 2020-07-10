74a5216b05cbb63a02e7d2c03e49683d01e95b74
Кирилл Курко <kirillkurko2609@gmail.com>
2020-07-10 09:25:47 +0300

    getRegexForIPv4 solution added.

--- task/05-regex-tasks.js
+++ task/05-regex-tasks.js
@@ -73,7 +73,8 @@ function getRegexForPitSpot() {
  * @return {RegExp}
  */
 function getRegexForIPv4() {
-   throw new Error('Not implemented');
+   var s = '(25[0-5]|2[0-4]\\d|[01]?\\d{1,2})';
+   return new RegExp(`^${s}\\.${s}\\.${s}\\.${s}$`);
 }
 
 
