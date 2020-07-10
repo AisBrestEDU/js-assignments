c2b2fb0d5ea69a54aaba19139aeaca79a9e6dd9b
Anna Balkovskaya <anna.balkovskaya@gmail.com>
2020-07-10 23:13:52 +0300

    Update 04-arrays-tasks.js

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -38,7 +38,7 @@ function findElement(arr, value) {
  *    5 => [ 1, 3, 5, 7, 9 ]
  */
 function generateOdds(len) {
-   const arr = new Array(len);
+   let arr = new Array(len);
    arr.fill(1);
    return arr.map((item, i) => i * 2 + 1);
 }
