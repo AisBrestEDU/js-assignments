17708c99b7169de04c4f7fc35e7b976e9778d174
PashaPunko <fpm.punko@bsu.by>
2020-07-11 17:21:13 +0300

    task 10 complete

--- task/10-katas-1-tasks.js
+++ task/10-katas-1-tasks.js
@@ -16,9 +16,90 @@
  *     { abbreviation : 'NbW',   azimuth : 348.75 }
  *  ]
  */
+
 function createCompassPoints() {
-    throw new Error('Not implemented');
     var sides = ['N','E','S','W'];  // use array of cardinal directions only!
+    class CompasPoint{
+        constructor(abbreviation, azimuth){
+            this.azimuth = azimuth<0?360+azimuth:azimuth;
+            this.abbreviation = abbreviation;
+        }
+    }
+
+    class StartBuilder{
+        buildNorthAndSouth(firstChar){
+            return [new CompasPoint(firstChar, sides.indexOf(firstChar)*90)]
+            .concat(new MiddleBuilder().buildNorthAndSouth(firstChar));
+        }
+        buildEastAndWest(firstChar){
+            return [new CompasPoint(firstChar, sides.indexOf(firstChar)*90)]
+            .concat(new MiddleBuilder().buildEastAndWest(firstChar));
+        }
+        
+    }
+
+    class MiddleBuilder{
+        buildNorthAndSouth(firstChar){
+            return new EndBuilder().build(firstChar+'b', 'b')
+            .concat(new EndBuilder().build(firstChar+firstChar, firstChar))
+            .concat(new EndBuilder().build(firstChar+sides[(sides.indexOf(firstChar)+1)%4], firstChar))
+            .concat(new EndBuilder().build(firstChar+sides[(sides.indexOf(firstChar)+3)%4], firstChar));
+        }
+        buildEastAndWest(firstChar){
+            let prevDirection = sides[(sides.indexOf(firstChar)+3)%4];
+            let nextDirection = sides[(sides.indexOf(firstChar)+1)%4];
+            return new EndBuilder().build(firstChar+'b', 'b')
+            .concat([new CompasPoint(firstChar+nextDirection+firstChar, 
+                                     sides.indexOf(firstChar)*90+22.5*this.countSign(firstChar, nextDirection))])
+            .concat([new CompasPoint(firstChar+prevDirection+firstChar, 
+                                     sides.indexOf(firstChar)*90+22.5*this.countSign(firstChar, prevDirection))])
+        }
+        
+        countSign(firstChar, curChar){
+            return Math.sign((sides.indexOf(curChar)+sides.indexOf(firstChar))%4-(sides.indexOf(curChar)+sides.indexOf(firstChar)+2)%4);
+        }
+        
+    }
+
+    class EndBuilder{
+        build(begin, endChar){
+            let startCharIndex = sides.indexOf(begin.charAt(0))
+            let prevDirection = sides[(sides.indexOf(endChar)+3)%4];
+            let nextDirection = sides[(sides.indexOf(endChar)+1)%4];
+            let prevDirectionForb = sides[(startCharIndex+3)%4];
+            let nextDirectionForb = sides[(startCharIndex+1)%4];
+            return begin.charAt(0) == begin.charAt(1) ? [new CompasPoint(begin+nextDirection, 
+                                                                         startCharIndex*90 + 22.5*this.countSign(begin.charAt(0), nextDirection)), 
+                                                        new CompasPoint( begin+prevDirection,
+                                                                         startCharIndex*90 + 22.5*this.countSign(begin.charAt(0), prevDirection))]:
+                    endChar == 'b' ? [new CompasPoint( begin+nextDirectionForb, 
+                                                       startCharIndex*90 + 11.25*this.countSign(begin.charAt(0), nextDirectionForb)), 
+                                    new CompasPoint( begin+prevDirectionForb,
+                                                       startCharIndex*90 + 11.25*this.countSign(begin.charAt(0), prevDirectionForb))]:
+                                    
+                                    [new CompasPoint( begin, 90*startCharIndex+45*this.countSign(begin.charAt(0), begin.charAt(1))),
+                                    new CompasPoint(begin+'b'+begin.charAt(0),
+                                                    90*startCharIndex+45*this.countSign(begin.charAt(0), begin.charAt(1)) 
+                                                        + 11.25*this.countHardSign(begin.charAt(0), begin.charAt(1), begin.charAt(0))), 
+                                    new CompasPoint(begin+'b'+begin.charAt(1),
+                                                    90*startCharIndex+45*this.countSign(begin.charAt(0), begin.charAt(1)) 
+                                                        + 11.25*this.countHardSign(begin.charAt(0), begin.charAt(1), begin.charAt(1)))]
+        }
+        countSign(firstChar, curChar){
+            let res=Math.sign((sides.indexOf(curChar)+sides.indexOf(firstChar)+2)%4-(sides.indexOf(curChar)+sides.indexOf(firstChar))%4)
+            return sides.indexOf(curChar)%2 == 1? res: -res;;
+        }
+        countHardSign(firstChar, secChar, thirdChar){
+            let sum =  sides.indexOf(firstChar)+sides.indexOf(thirdChar)+sides.indexOf(secChar);
+            return sum <3 || sum>6 
+                            ? sum%2==1?-1:1
+                            : sum%2==0?-1:1;
+        }
+    }
+    return (new StartBuilder().buildNorthAndSouth('N')
+            .concat(new StartBuilder().buildNorthAndSouth('S')
+            .concat(new StartBuilder().buildEastAndWest('E')
+            .concat(new StartBuilder().buildEastAndWest('W'))))).sort((l, r) => l.azimuth-r.azimuth)
 }
 
 
@@ -56,7 +137,29 @@ function createCompassPoints() {
  *   'nothing to do' => 'nothing to do'
  */
 function* expandBraces(str) {
-    throw new Error('Not implemented');
+    let ans = Array(1).fill(str);
+    let f = true;
+    while (f){
+        let len = ans.length
+        f = false;
+        for (let i = 0; i< len; i++){
+            let executions = /{([^{}]+)}/.exec(ans[i]); 
+            if (executions){
+                f = true;
+                let variants = executions[1].split(',');
+                for (let j = 1;j< variants.length; j++){
+                    ans.push(ans[i].replace(executions[0], variants[j]));
+                }
+                ans[i] = ans[i].replace(executions[0], variants[0]);
+            }
+            else f|=false;
+        }
+        
+    }
+    ans = ans.filter((x, idx)=>ans.indexOf(x) == idx)
+    for (let el of ans){
+        yield el;
+    }
 }
 
 
@@ -88,7 +191,18 @@ function* expandBraces(str) {
  *
  */
 function getZigZagMatrix(n) {
-    throw new Error('Not implemented');
+    let ans = Array(n).fill(0).map((x)=>Array(n).fill(0))
+    let counter = 0, i=0, j=0, delta = 1;
+    for (let  k = 0; k<(n*2-1);k++) {
+        [j,i] = k%2 == 1 ? [Math.min(k,n-1), k -Math.min(k,n-1) ]:[k -Math.min(k,n-1),Math.min(k,n-1)]; 
+        while (i>=0 && j>=0 && j<n && i<n){
+            ans[i][j] = counter++;
+            j+=delta;
+            i-=delta;
+        }
+        delta*=-1;
+    }
+    return ans;
 }
 
 
@@ -113,7 +227,20 @@ function getZigZagMatrix(n) {
  *
  */
 function canDominoesMakeRow(dominoes) {
-    throw new Error('Not implemented');
+    let maxVal=dominoes.reduce((acc, val)=> Math.max(Math.max(val[1], val[0]), acc),0);
+    let matrixOfNe = Array(maxVal+1).fill(0).map((x)=>Array(maxVal+1).fill(0));
+    
+    for (let el of dominoes){
+        matrixOfNe[el[0]][el[1]]++;
+        matrixOfNe[el[1]][el[0]]++;
+    }
+    for (let i=0; i<matrixOfNe.length; i++){
+        if (matrixOfNe[i][i]!=0 && matrixOfNe[i][i] == matrixOfNe[i].reduce((acc, val)=>acc+val,0)) return false;
+    }
+    
+    if (matrixOfNe.reduce((acc1,val1)=>acc1+val1.reduce((acc2, val2)=>acc2+val2,0)%2,0) >2) return false;
+    return true;
+    
 }
 
 
@@ -136,8 +263,30 @@ function canDominoesMakeRow(dominoes) {
  * [ 0, 1, 2, 5, 7, 8, 9] => '0-2,5,7-9'
  * [ 1, 2, 4, 5]          => '1,2,4,5'
  */
+
+function accom(acc, val){
+    if ( acc[acc.length-1]+1 == val || acc.length == 0){
+        acc.push(val);
+        return acc;
+    }
+    else{
+        
+        return acc;
+    }
+}
+
 function extractRanges(nums) {
-    throw new Error('Not implemented');
+    let ans = nums.join(',');
+    let curIdx = 0;
+    while (curIdx < nums.length){
+        let range = nums.slice(curIdx).reduce((acc,val) => accom(acc, val), []);
+        if (range.length>2){
+            ans = ans.replace(range.join(','), `${range[0]}-${range[range.length-1]}`)
+            curIdx = nums.indexOf(range[range.length-1])+1;
+        }
+        else curIdx++;
+    }
+    return ans;
 }
 
 module.exports = {
