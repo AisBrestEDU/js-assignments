3f635765d87a7765116faa1cb04fbe765e532198
Кирилл Курко <kirillkurko2609@gmail.com>
2020-07-12 14:45:19 +0300

    extractNameFromTemplate solution added.

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -69,7 +69,7 @@ function getStringFromTemplate(firstName, lastName) {
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
 function extractNameFromTemplate(value) {
-    throw new Error('Not implemented');
+    return value.substr(7, value.length - 8);
 }
 
 
