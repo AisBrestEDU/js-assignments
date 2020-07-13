7f85e9809cc617cd27f2ad85b728489f0b18eb5c
Maksim Litvyakov <litvy-maksim@yandex.ru>
2020-07-13 12:14:20 +0700

    getFalsyValuesCount done

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -376,7 +376,7 @@ function getItemsSum(arr) {
  *  [ null, undefined, NaN, false, 0, '' ]  => 6
  */
 function getFalsyValuesCount(arr) {
-    throw new Error('Not implemented');
+    return arr.filter(item => !item).length;
 }
 
 /**
