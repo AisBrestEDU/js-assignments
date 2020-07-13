1bde9465b69cd281a6b9af30a6f2fdd5a1c14784
Maksim Litvyakov <litvy-maksim@yandex.ru>
2020-07-13 12:04:46 +0700

    getItemsSum done

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -356,7 +356,11 @@ function sortDigitNamesByNumericOrder(arr) {
  *   [ 1, 10, 100, 1000 ]  => 1111
  */
 function getItemsSum(arr) {
-    throw new Error('Not implemented');
+    if (arr.length > 0) {
+        return arr.reduce((a, b) => a + b);
+    } else {
+        return 0;
+    }
 }
 
 /** 
