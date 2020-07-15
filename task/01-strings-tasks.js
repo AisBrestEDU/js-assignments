651e4886b5d5aa93fc04fa0f96af48990d6ff73d
Stefffa-code <61387139+Stefffa-code@users.noreply.github.com>
2020-07-15 12:34:32 +0300

    01 task

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -22,7 +22,7 @@
  *   '',  'bb'  => 'bb'
  */
 function concatenateStrings(value1, value2) {
-    throw new Error('Not implemented');
+     return value1 + value2 
 }
 
 
@@ -38,7 +38,7 @@ function concatenateStrings(value1, value2) {
  *   ''      => 0
  */
 function getStringLength(value) {
-    throw new Error('Not implemented');
+    return value.length
 }
 
 /**
@@ -55,7 +55,7 @@ function getStringLength(value) {
  *   'Chuck','Norris'  => 'Hello, Chuck Norris!'
  */
 function getStringFromTemplate(firstName, lastName) {
-    throw new Error('Not implemented');
+    return `Hello, ${firstName} ${lastName}!`
 }
 
 /**
@@ -69,7 +69,8 @@ function getStringFromTemplate(firstName, lastName) {
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
 function extractNameFromTemplate(value) {
-    throw new Error('Not implemented');
+    let val = value.replace("!", "")   
+    return val.slice(7)
 }
 
 
@@ -84,7 +85,7 @@ function extractNameFromTemplate(value) {
  *   'cat'       => 'c'
  */
 function getFirstChar(value) {
-    throw new Error('Not implemented');
+    return value.charAt(0)
 }
 
 /**
@@ -99,7 +100,7 @@ function getFirstChar(value) {
  *   '\tHello, World! ' => 'Hello, World!'
  */
 function removeLeadingAndTrailingWhitespaces(value) {
-    throw new Error('Not implemented');
+    return value.trim()
 }
 
 /**
@@ -114,7 +115,11 @@ function removeLeadingAndTrailingWhitespaces(value) {
  *   'cat', 3 => 'catcatcat'
  */
 function repeatString(value, count) {
-    throw new Error('Not implemented');
+    let str = ""
+    for (let i = 0; i < count; i++){ 
+        str = str + value
+    }
+    return str
 }
 
 /**
@@ -130,7 +135,7 @@ function repeatString(value, count) {
  *   'ABABAB','BA' => 'ABAB'
  */
 function removeFirstOccurrences(str, value) {
-    throw new Error('Not implemented');
+    return str.replace(value, "")
 }
 
 /**
@@ -145,7 +150,7 @@ function removeFirstOccurrences(str, value) {
  *   '<a>' => 'a'
  */
 function unbracketTag(str) {
-    throw new Error('Not implemented');
+    return str.replace(/[<>]/g, "");
 }
 
 
@@ -160,7 +165,7 @@ function unbracketTag(str) {
  *  'abcdefghijklmnopqrstuvwxyz' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
  */
 function convertToUpperCase(str) {
-    throw new Error('Not implemented');
+    return str.toUpperCase()
 }
 
 /**
@@ -174,7 +179,7 @@ function convertToUpperCase(str) {
  *   'info@gmail.com' => ['info@gmail.com']
  */
 function extractEmails(str) {
-    throw new Error('Not implemented');
+    return str.split(';')
 }
 
 /**
@@ -201,7 +206,20 @@ function extractEmails(str) {
  *
  */
 function getRectangleString(width, height) {
-    throw new Error('Not implemented');
+    let str = ""
+    for(let h = 0; h < height; h++ ) {
+        for(let w = 0; w < width; w++) {              
+          if (h==0 && w == 0) str += '┌'
+          else if (h==0 && w == width-1) str += '┐'
+          else if (h==height-1 && w == 0) str += '└'
+          else if (h==height-1 && w == width-1) str += '┘'
+          else if (w==0 || w == width-1) str += '│'
+          else if (h==0 || h == height-1) str += '─'
+          else str += ' '
+        }
+        str += "\n"
+    }
+    return str
 }
 
 
@@ -221,7 +239,9 @@ function getRectangleString(width, height) {
  *
  */
 function encodeToRot13(str) {
-    throw new Error('Not implemented');
+    return str.replace(/[A-Za-z]/g, function (c) {
+        return String.fromCodePoint((((c = c.charCodeAt(0)) & 223) - 52) % 26 + (c & 32) + 65);
+    });
 }
 
 /**
@@ -238,7 +258,7 @@ function encodeToRot13(str) {
  *   isString(new String('test')) => true
  */
 function isString(value) {
-    throw new Error('Not implemented');
+    return (typeof value === "string" || value instanceof String)
 }
 
 
@@ -267,7 +287,22 @@ function isString(value) {
  *   'K♠' => 51
  */
 function getCardId(value) {
-    throw new Error('Not implemented');
+    let ranks = ['A','2','3','4','5','6','7','8','9','10','J','Q','K']
+    let suits = ['♣', '♦', '♥', '♠']
+    let cards = []
+    let mass = new Map([])
+
+    for (let num = 0; num<suits.length; num++){
+        for (let i = 0; i<ranks.length; i++){
+            cards.push(ranks[i]+suits[num])   
+        }
+    }
+
+    for (let i = 0; i<cards.length; i++){
+        mass.set(cards[i], i)  
+    }
+    
+    return mass.get(value)
 }
 
 
