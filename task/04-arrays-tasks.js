c377bc60445f760284674989e6f14b21d43f881c
ArtemOsadchuck <ArtemOsadchuck@users.noreply.github.com>
2020-07-16 23:02:20 +0300

    Update 04-arrays-tasks.js

--- task/04-arrays-tasks.js
+++ task/04-arrays-tasks.js
@@ -560,7 +560,8 @@ function group(array, keySelector, valueSelector) {
  *   ['one','two','three'], x=>x.split('')  =>   ['o','n','e','t','w','o','t','h','r','e','e']
  */
 function selectMany(arr, childrenSelector) {
-    return arr.flatMap(childrenSelector)
+    //return arr.flatMap(childrenSelector)
+    return [].concat(...arr.map(childrenSelector))
 }
 
 
