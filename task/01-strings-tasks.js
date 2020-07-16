ac8e62ecedb575b03c23671f0a8d3b0871509dd0
Andrei Miashkou <meshkow.andrei@gmail.com>
2020-07-16 13:33:51 +0300

    fix: fix change in task_1_4

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 /********************************************************************************************
@@ -22,7 +23,7 @@
  *   '',  'bb'  => 'bb'
  */
 function concatenateStrings(value1, value2) {
-	return value1 + value2;
+	throw new Error('Not implemented');
 }
 
 
@@ -38,7 +39,7 @@ function concatenateStrings(value1, value2) {
  *   ''      => 0
  */
 function getStringLength(value) {
-	return value.length;
+	throw new Error('Not implemented');
 }
 
 /**
@@ -55,7 +56,7 @@ function getStringLength(value) {
  *   'Chuck','Norris'  => 'Hello, Chuck Norris!'
  */
 function getStringFromTemplate(firstName, lastName) {
-	return `Hello, ${firstName} ${lastName}!`;
+	throw new Error('Not implemented');
 }
 
 /**
@@ -69,7 +70,7 @@ function getStringFromTemplate(firstName, lastName) {
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
 function extractNameFromTemplate(value) {
-	return value.slice(7, -1);
+	throw new Error('Not implemented');
 }
 
 
@@ -84,7 +85,7 @@ function extractNameFromTemplate(value) {
  *   'cat'       => 'c'
  */
 function getFirstChar(value) {
-	return value[0];
+	throw new Error('Not implemented');
 }
 
 /**
@@ -99,9 +100,8 @@ function getFirstChar(value) {
  *   '\tHello, World! ' => 'Hello, World!'
  */
 function removeLeadingAndTrailingWhitespaces(value) {
-	return value.trim();
+	throw new Error('Not implemented');
 }
-
 /**
  * Returns a string that repeated the specified number of times.
  *
@@ -114,7 +114,7 @@ function removeLeadingAndTrailingWhitespaces(value) {
  *   'cat', 3 => 'catcatcat'
  */
 function repeatString(value, count) {
-	return value.repeat(count);
+	throw new Error('Not implemented');
 }
 
 /**
@@ -130,7 +130,7 @@ function repeatString(value, count) {
  *   'ABABAB','BA' => 'ABAB'
  */
 function removeFirstOccurrences(str, value) {
-	return str.replace(value, '');
+	throw new Error('Not implemented');
 }
 
 /**
@@ -145,7 +145,7 @@ function removeFirstOccurrences(str, value) {
  *   '<a>' => 'a'
  */
 function unbracketTag(str) {
-	return str.slice(1, -1);
+	throw new Error('Not implemented');
 }
 
 
@@ -160,7 +160,7 @@ function unbracketTag(str) {
  *  'abcdefghijklmnopqrstuvwxyz' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
  */
 function convertToUpperCase(str) {
-	return str.toUpperCase();
+	throw new Error('Not implemented');
 }
 
 /**
@@ -174,7 +174,7 @@ function convertToUpperCase(str) {
  *   'info@gmail.com' => ['info@gmail.com']
  */
 function extractEmails(str) {
-	return str.split(';');
+	throw new Error('Not implemented');
 }
 
 /**
@@ -201,7 +201,7 @@ function extractEmails(str) {
  *
  */
 function getRectangleString(width, height) {
-	return "┌" + "─".repeat(width - 2) + "┐\n" + ("│" + " ".repeat(width - 2) + "│\n").repeat(height - 2) + "└" + "─".repeat(width - 2) + "┘\n";
+	throw new Error('Not implemented');
 }
 
 
@@ -221,11 +221,7 @@ function getRectangleString(width, height) {
  *
  */
 function encodeToRot13(str) {
-	const input = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
-	const output = 'NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm';
-	let index = x => input.indexOf(x);
-	let translate = x => index(x) > -1 ? output[index(x)] : x;
-	return str.split('').map(translate).join('');
+	throw new Error('Not implemented');
 }
 
 /**
@@ -242,7 +238,7 @@ function encodeToRot13(str) {
  *   isString(new String('test')) => true
  */
 function isString(value) {
-	return typeof (value) === 'string' || value instanceof String;
+	throw new Error('Not implemented');
 }
 
 
@@ -271,11 +267,7 @@ function isString(value) {
  *   'K♠' => 51
  */
 function getCardId(value) {
-	const cards = ['A♣', '2♣', '3♣', '4♣', '5♣', '6♣', '7♣', '8♣', '9♣', '10♣', 'J♣', 'Q♣', 'K♣',
-		'A♦', '2♦', '3♦', '4♦', '5♦', '6♦', '7♦', '8♦', '9♦', '10♦', 'J♦', 'Q♦', 'K♦',
-		'A♥', '2♥', '3♥', '4♥', '5♥', '6♥', '7♥', '8♥', '9♥', '10♥', 'J♥', 'Q♥', 'K♥',
-		'A♠', '2♠', '3♠', '4♠', '5♠', '6♠', '7♠', '8♠', '9♠', '10♠', 'J♠', 'Q♠', 'K♠'];
-	return cards.indexOf(value);
+	throw new Error('Not implemented');
 }
 
 
--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -1,3 +1,4 @@
+
 'use strict';
 
 /********************************************************************************************
@@ -22,7 +23,7 @@
  *   5, 5  => 25
  */
 function getRectangleArea(width, height) {
-	return width * height;
+	throw new Error('Not implemented');
 }
 
 
@@ -38,7 +39,7 @@ function getRectangleArea(width, height) {
  *   0    => 0
  */
 function getCicleCircumference(radius) {
-	return radius * 2 * Math.PI;
+	throw new Error('Not implemented');
 }
 
 /**
@@ -54,7 +55,7 @@ function getCicleCircumference(radius) {
  *  -3, 3  => 0
  */
 function getAverage(value1, value2) {
-	return value1 / 2 + value2 / 2;
+	throw new Error('Not implemented');
 }
 
 /**
@@ -73,7 +74,7 @@ function getAverage(value1, value2) {
  *   (-5,0) (10,-10) => 18.027756377319946
  */
 function getDistanceBetweenPoints(x1, y1, x2, y2) {
-	return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+	throw new Error('Not implemented');
 }
 
 /**
@@ -89,7 +90,7 @@ function getDistanceBetweenPoints(x1, y1, x2, y2) {
  *   5*x = 0         => 0
  */
 function getLinearEquationRoot(a, b) {
-	return -b / a;
+	throw new Error('Not implemented');
 }
 
 
@@ -111,7 +112,7 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (1,2)     => 0
  */
 function getAngleBetweenVectors(x1, y1, x2, y2) {
-	return Math.acos((x1 * x2 + y1 * y2) / Math.sqrt(Math.pow(x1, 2) + Math.pow(y1, 2)) * Math.sqrt(Math.pow(x2, 2) + Math.pow(y2, 2)));
+	throw new Error('Not implemented');
 }
 
 /**
@@ -127,7 +128,7 @@ function getAngleBetweenVectors(x1, y1, x2, y2) {
  *     0     => 0
  */
 function getLastDigit(value) {
-	return value % 10;
+	throw new Error('Not implemented');
 }
 
 
@@ -143,7 +144,7 @@ function getLastDigit(value) {
  * '-525.5'     => -525.5
  */
 function parseNumberFromString(value) {
-	return Number(value);
+	throw new Error('Not implemented');
 }
 
 /**
@@ -160,7 +161,7 @@ function parseNumberFromString(value) {
  *   1,2,3   => 3.741657386773941
  */
 function getParallelipidedDiagonal(a, b, c) {
-	return Math.sqrt(a * a + b * b + c * c);
+	throw new Error('Not implemented');
 }
 
 /**
@@ -181,7 +182,7 @@ function getParallelipidedDiagonal(a, b, c) {
  *   1678, 3  => 2000
  */
 function roundToPowerOfTen(num, pow) {
-	return Math.round(num / Math.pow(10, pow)) * Math.pow(10, pow);
+	throw new Error('Not implemented');
 }
 
 /**
@@ -202,13 +203,7 @@ function roundToPowerOfTen(num, pow) {
  *   17 => true
  */
 function isPrime(n) {
-	for (let i = 2; i <= Math.sqrt(n);) {
-		if (n % i == 0) {
-			return false;
-		}
-		i === 2 ? i++ : i += 2;
-	}
-	return n > 1;
+	throw new Error('Not implemented');
 }
 
 /**
@@ -227,7 +222,7 @@ function isPrime(n) {
  *   toNumber(new Number(42), 0) => 42
  */
 function toNumber(value, def) {
-	return Number(value) ? Number(value) : def
+	throw new Error('Not implemented');
 }
 
 module.exports = {
