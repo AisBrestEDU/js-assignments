2e1283c9318cf75140e3589d390ce26cf4eaffbc
Viktor <ecartman41@gmail.com>
2020-07-17 00:51:59 +0300

    remarks

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -123,15 +123,13 @@ function getLinearEquationRoot(a, b) {
  */
 function getAngleBetweenVectors(x1, y1, x2, y2) {
   //  throw new Error('Not implemented');
-  let a= (x1*x2 + y1    *y2) ;
+  let a= (x1*x2 + y1*y2) ;
+  let b = 0;
 
-let b = 0;
-
-  let c =  ( Math.sqrt(x1**2 + y1**2)) * ( Math.sqrt(x2**2 + y2**2)  ) ;
-
+  //let c =  (Math.sqrt(x1**2 + y1**2)) * (Math.sqrt(x2**2 + y2**2)  ) ;
+  let c = (Math.sqrt(x1**2 + y1**2)) * (Math.sqrt(x2**2 +y2**2)) ;
  
-  b = a/c;
-  
+  b = a/c;  
 
   return  Math.acos(b);
 }
