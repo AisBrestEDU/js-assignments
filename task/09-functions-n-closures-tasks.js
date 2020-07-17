5278f3a77e140e9c840481f51d6a62e0bbaaef91
Dzianis <imxrayz@gmail.com>
2020-07-17 13:15:19 +0300

    fix args

--- task/09-functions-n-closures-tasks.js
+++ task/09-functions-n-closures-tasks.js
@@ -154,11 +154,11 @@ function retry(func, attempts) {
  *
  */
 function logger(func, logFunc) {
-    return function (...args) {
-        const logStart = `${func.name}(${args.map(value => JSON.stringify(value))}) starts`;
-        const logEnd = `${func.name}(${args.map(value => JSON.stringify(value))}) ends`;
+    return function () {
+        const logStart = `${func.name}(${[...arguments].map(value => JSON.stringify(value))}) starts`;
+        const logEnd = `${func.name}(${[...arguments].map(value => JSON.stringify(value))}) ends`;
         logFunc(logStart);
-        const result = func(...args)
+        const result = func(...arguments)
         logFunc(logEnd);
         return result;
     }
@@ -181,11 +181,17 @@ function logger(func, logFunc) {
  */
 function partialUsingArguments(fn) {
     let array1 = [...arguments];
+    array1.splice(array1.findIndex(fn), 1);
     return (...args) => {
-        array1.splice(array1.findIndex(fn), 1);
-        let array = [...array1, ...args];
+        let arr = [...args];
+        let array = [...array1, ...arr];
         return fn(...array);
     }
+
+    // let args = [...arguments].slice(1);
+    // return function () {
+    //     return args.concat([...arguments]).join('');
+    // };
     throw new Error('Not implemented');
 }
 
@@ -208,7 +214,6 @@ function partialUsingArguments(fn) {
  */
 function getIdGeneratorFunction(startFrom) {
     let id = startFrom;
-    
     return function () {
         function* generateId(){           
             while(true)
