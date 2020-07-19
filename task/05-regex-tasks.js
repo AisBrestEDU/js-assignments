2cfd9cff968147f112f9d6c427b60983f2d8bf6a
Alex Malyshko <slashhem@gmail.com>
2020-07-19 14:38:38 +0300

    Completed task 5

--- task/05-regex-tasks.js
+++ task/05-regex-tasks.js
@@ -31,7 +31,7 @@
  * @return {RegExp}
  */
 function getRegexForGuid() {
-   throw new Error('Not implemented');
+   return /{[0-9a-f]{8}(-[0-9a-f]{4}){3}-[0-9a-f]{12}}/i;
 }
 
 
@@ -53,7 +53,7 @@ function getRegexForGuid() {
  *
  */
 function getRegexForPitSpot() {
-   throw new Error('Not implemented');
+   return /p.t/;
 }
 
 
@@ -72,7 +72,7 @@ function getRegexForPitSpot() {
  * @return {RegExp}
  */
 function getRegexForIPv4() {
-   throw new Error('Not implemented');
+   return /^((2[0-5]\d|[01]\d{2}|\d{2}|\d)\.){3}(2[0-5]\d|[01]\d{2}|\d{2}|\d)$/;
 }
 
 
@@ -91,7 +91,7 @@ function getRegexForIPv4() {
  * @return {RegExp}
  */
 function getRegexForSSN() {
-   throw new Error('Not implemented');
+   return /\d{2}[1-9]-\d[1-9]-\d{3}[1-9]/;
 }
 
 
@@ -116,7 +116,7 @@ function getRegexForSSN() {
  *   'Pa55'.match(validator) => false
  */
 function getPasswordValidator(minLength) {
-   throw new Error('Not implemented');
+   return new RegExp(`^(?=.*[0-9]+.*)(?=.*[A-Z]+.*)(?=.*[a-z]+.*)[0-9A-Za-z]{${minLength},}`);
 }
 
 
