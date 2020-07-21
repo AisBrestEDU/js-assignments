75967e1e69358b59f66fcdfcce97cec44d2d1db8
AlexErm0 <ermolaevaleks26@gmail.com>
2020-07-21 19:29:25 +0300

    reviewed mistakes

--- task/07-yield-tasks.js
+++ task/07-yield-tasks.js
@@ -1,32 +1,32 @@
 'use strict';
 
 /********************************************************************************************
- *                                                                                          *
- * Plese read the following tutorial before implementing tasks:                             *
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Iterators_and_Generators   *
- * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/yield        *
- *                                                                                          *
+ * *
+ * Plese read the following tutorial before implementing tasks: *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Iterators_and_Generators *
+ * https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/yield *
+ * *
  ********************************************************************************************/
 
 
 /**
  * Returns the lines sequence of "99 Bottles of Beer" song:
  *
- *  '99 bottles of beer on the wall, 99 bottles of beer.'
- *  'Take one down and pass it around, 98 bottles of beer on the wall.'
- *  '98 bottles of beer on the wall, 98 bottles of beer.'
- *  'Take one down and pass it around, 97 bottles of beer on the wall.'
- *  ...
- *  '1 bottle of beer on the wall, 1 bottle of beer.'
- *  'Take one down and pass it around, no more bottles of beer on the wall.'
- *  'No more bottles of beer on the wall, no more bottles of beer.'
- *  'Go to the store and buy some more, 99 bottles of beer on the wall.'
+ * '99 bottles of beer on the wall, 99 bottles of beer.'
+ * 'Take one down and pass it around, 98 bottles of beer on the wall.'
+ * '98 bottles of beer on the wall, 98 bottles of beer.'
+ * 'Take one down and pass it around, 97 bottles of beer on the wall.'
+ * ...
+ * '1 bottle of beer on the wall, 1 bottle of beer.'
+ * 'Take one down and pass it around, no more bottles of beer on the wall.'
+ * 'No more bottles of beer on the wall, no more bottles of beer.'
+ * 'Go to the store and buy some more, 99 bottles of beer on the wall.'
  *
  * See the full text at
  * http://99-bottles-of-beer.net/lyrics.html
  *
  * NOTE: Please try to complete this task faster then original song finished:
- * https://www.youtube.com/watch?v=Z7bmyjxJuVY   :)
+ * https://www.youtube.com/watch?v=Z7bmyjxJuVY :)
  *
  *
  * @return {Iterable.<string>}
@@ -47,7 +47,7 @@ function* get99BottlesOfBeer() {
 
 /**
  * Returns the Fibonacci sequence:
- *   0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
+ * 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, ...
  *
  * See more at: https://en.wikipedia.org/wiki/Fibonacci_number
  *
@@ -55,9 +55,9 @@ function* get99BottlesOfBeer() {
  *
  */
 function* getFibonacciSequence() {
-	var past1 = 0;
-	var past2 = 1;
-	var num = past1;
+	let past1 = 0;
+	let past2 = 1;
+	let num = past1;
 	while(true){
 		past1=past2;
 		past2=num;
@@ -78,34 +78,34 @@ function* getFibonacciSequence() {
  * @return {Iterable.<object>} the sequence of all tree nodes in depth-first order
  * @example
  *
- *   var node1 = { n:1 }, node2 = { n:2 }, node3 = { n:3 }, node4 = { n:4 },
- *       node5 = { n:5 }, node6 = { n:6 }, node7 = { n:7 }, node8 = { n:8 };
- *   node1.children = [ node2, node6, node7 ];
- *   node2.children = [ node3, node4 ];
- *   node4.children = [ node5 ];
- *   node7.children = [ node8 ];
+ * var node1 = { n:1 }, node2 = { n:2 }, node3 = { n:3 }, node4 = { n:4 },
+ * node5 = { n:5 }, node6 = { n:6 }, node7 = { n:7 }, node8 = { n:8 };
+ * node1.children = [ node2, node6, node7 ];
+ * node2.children = [ node3, node4 ];
+ * node4.children = [ node5 ];
+ * node7.children = [ node8 ];
  *
- *     source tree (root = 1):
- *            1
- *          / | \
- *         2  6  7
- *        / \     \            =>    { 1, 2, 3, 4, 5, 6, 7, 8 }
- *       3   4     8
- *           |
- *           5
+ * source tree (root = 1):
+ * 1
+ * / | \
+ * 2 6 7
+ * / \ \ => { 1, 2, 3, 4, 5, 6, 7, 8 }
+ * 3 4 8
+ * |
+ * 5
  *
- *  depthTraversalTree(node1) => node1, node2, node3, node4, node5, node6, node7, node8
+ * depthTraversalTree(node1) => node1, node2, node3, node4, node5, node6, node7, node8
  *
  */
 function* depthTraversalTree(root) {
 	var cash = [root];
-    while (cash.length!=0) {
-        root = cash.pop();
+ while (cash.length!=0) {
+ root = cash.pop();
 		yield root;
 		if(root.children){
 			for(let i of root.children.reverse()) cash.push(i);			
 		}
-    }
+ }
 }
 
 
@@ -119,28 +119,28 @@ function* depthTraversalTree(root) {
  * @params {object} root the tree root
  * @return {Iterable.<object>} the sequence of all tree nodes in breadth-first order
  * @example
- *     source tree (root = 1):
+ * source tree (root = 1):
  *
- *            1
- *          / | \
- *         2  3  4
- *        / \     \            =>    { 1, 2, 3, 4, 5, 6, 7, 8 }
- *       5   6     7
- *           |
- *           8
+ * 1
+ * / | \
+ * 2 3 4
+ * / \ \ => { 1, 2, 3, 4, 5, 6, 7, 8 }
+ * 5 6 7
+ * |
+ * 8
  *
  */
 function* breadthTraversalTree(root) {
 	var cash = [root];
 	var flag = 0;
-    while (flag<cash.length) {
-        root = cash[flag];
+	while (flag<cash.length) {
+		root = cash[flag];
 		yield root;
 		if(root.children){
-			for(let i of root.children) cash.push(i);			
+			for(let i of root.children) cash.push(i);
 		}
 		flag++;
-    }
+	}
 }
 
 
@@ -153,33 +153,27 @@ function* breadthTraversalTree(root) {
  * @return {Iterable.<number>} the merged sorted sequence
  *
  * @example
- *   [ 1, 3, 5, ... ], [2, 4, 6, ... ]  => [ 1, 2, 3, 4, 5, 6, ... ]
- *   [ 0 ], [ 2, 4, 6, ... ]  => [ 0, 2, 4, 6, ... ]
- *   [ 1, 3, 5, ... ], [ -1 ] => [ -1, 1, 3, 5, ...]
+ * [ 1, 3, 5, ... ], [2, 4, 6, ... ] => [ 1, 2, 3, 4, 5, 6, ... ]
+ * [ 0 ], [ 2, 4, 6, ... ] => [ 0, 2, 4, 6, ... ]
+ * [ 1, 3, 5, ... ], [ -1 ] => [ -1, 1, 3, 5, ...]
  */
 function* mergeSortedSequences(source1, source2) {
 	let start1 = source1();
-    let start2 = source2();
-    let mass1 = start1.next()
-    let mass2 = start2.next()
-    while(!mass1.done || !mass2.done){
-        if(mass1.value > mass2.value){
-            if(mass2.value != undefined)
-                yield mass2.value
-            if(mass1.value != undefined)
-                yield mass1.value
-        }
-        else{
-            if(mass1.value != undefined)
-                yield mass1.value
-            if(mass2.value != undefined)
-                yield mass2.value
-
-        }
-
-        mass1 = start1.next()
-        mass2 = start2.next()
-    }
+	let start2 = source2();
+	let mass1 = start1.next()
+	let mass2 = start2.next()
+	while(!mass1.done || !mass2.done){
+		if(mass1.value > mass2.value){
+			if(mass2.value != undefined)yield mass2.value
+			if(mass1.value != undefined)yield mass1.value
+		}
+		else{
+			if(mass1.value != undefined)yield mass1.value
+			if(mass2.value != undefined)yield mass2.value
+		}
+		mass1 = start1.next()
+		mass2 = start2.next()
+	}
 }
 
 /**
@@ -189,40 +183,37 @@ function* mergeSortedSequences(source1, source2) {
  * @return {Promise} Promise with value returned via return 
  *
  * @example
- *   async((function*() {
- *      var a = yield new Promise((resolve)=> setTimeout(()=>resolve(5)));
- *      var b = yield Promise.resolve(6);
- *      return a + b;
- *   }).then(value=>console.log(value))  => 11
+ * async((function*() {
+ * var a = yield new Promise((resolve)=> setTimeout(()=>resolve(5)));
+ * var b = yield Promise.resolve(6);
+ * return a + b;
+ * }).then(value=>console.log(value)) => 11
  *
- *   Most popular implementation of the logic in npm https://www.npmjs.com/package/co
+ * Most popular implementation of the logic in npm https://www.npmjs.com/package/co
  */
 function async(generator) {
 	let prom = generator();
 	let task = prom.next();
-	
 	return task.value.then(() => {
-        let promise = Array();
-        try{
-            promise.push(task.value)
-            while(1)promise.push(prom.next().value)
-        }
-        finally{
-            return Promise.all(promise).then(x => {
-                return x.reduce((accumulator, init) => {
-                    return accumulator + init
-                }, 0)
-            })
-        }
-    })
+		let promise = Array();
+		try{
+			promise.push(task.value)
+			while(1)promise.push(prom.next().value)
+		}
+		finally{
+			return Promise.all(promise).then(x => {
+				return x.reduce((accumulator, init) => {return accumulator + init}, 0)
+			})
+		}
+	})
 }
 
 
 module.exports = {
-    get99BottlesOfBeer: get99BottlesOfBeer,
-    getFibonacciSequence: getFibonacciSequence,
-    depthTraversalTree: depthTraversalTree,
-    breadthTraversalTree: breadthTraversalTree,
-    mergeSortedSequences: mergeSortedSequences,
-    async               : async
+ get99BottlesOfBeer: get99BottlesOfBeer,
+ getFibonacciSequence: getFibonacciSequence,
+ depthTraversalTree: depthTraversalTree,
+ breadthTraversalTree: breadthTraversalTree,
+ mergeSortedSequences: mergeSortedSequences,
+ async : async
 };
