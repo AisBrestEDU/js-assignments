7dcd5aa410412a618dc5562fe2a0221b362f43e9
GoodMan <34167434+AlexandrKolyadich@users.noreply.github.com>
2020-07-22 13:45:55 +0300

    Update 02-numbers-tasks.js

--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -221,7 +221,7 @@ function isPrime(n) {
  *   toNumber(new Number(42), 0) => 42
  */
 function toNumber(value, def) {
-   throw new Error('Not implemented');
+  throw new Error('Not implemented');
 }
 
 module.exports = {
