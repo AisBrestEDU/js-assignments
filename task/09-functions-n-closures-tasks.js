146c2de5f85329a146d165712297ba751715183c
Sergey Samusik <s.samusik@outlook.com>
2020-07-25 22:46:59 +0300

    09-functions-n-closures-tasks: completed

--- task/09-functions-n-closures-tasks.js
+++ task/09-functions-n-closures-tasks.js
@@ -65,7 +65,13 @@ function getPowerFunction(exponent) {
  *   getPolynom()      => null
  */
 function getPolynom() {
-    throw new Error('Not implemented');
+    return (x) => {
+        let result = 0;
+        for (let i = 0; i < arguments.length; i++) {
+            result += arguments[i] * Math.pow(x, arguments.length - i - 1);
+        }
+        return result;
+    }
 }
 
 
@@ -105,7 +111,18 @@ function memoize(func) {
  * retryer() => 2
  */
 function retry(func, attempts) {
-    throw new Error('Not implemented');
+    return () => {
+        while (attempts-- > 0) {
+            try {
+                return func();
+            }
+            catch (err) {
+                if (attempts === 0) {
+                    throw err;
+                }
+            }
+        }
+    }
 }
 
 
@@ -133,7 +150,13 @@ function retry(func, attempts) {
  *
  */
 function logger(func, logFunc) {
-    throw new Error('Not implemented');
+    return function () {
+        let args = JSON.stringify(Array.from(arguments)).slice(1, -1);
+        logFunc(`${func.name}(${args}) starts`);
+        let res = func.apply(this, arguments);
+        logFunc(`${func.name}(${args}) ends`);
+        return res;
+    }
 }
 
 
@@ -151,7 +174,10 @@ function logger(func, logFunc) {
  *   partialUsingArguments(fn, 'a','b','c','d')() => 'abcd'
  */
 function partialUsingArguments(fn) {
-    throw new Error('Not implemented');
+    let args = [...arguments].slice(1);
+    return function () {
+        return fn.apply(null, args.concat([...arguments]));
+    }
 }
 
 
