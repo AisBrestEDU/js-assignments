1504b9355eeefc84fc7f8c16491218cf12db4ce2
Stepan Sukach <stepansukac@gmail.com>
2020-07-26 19:04:14 +0300

    refactor code

--- task/07-yield-tasks.js
+++ task/07-yield-tasks.js
@@ -33,15 +33,16 @@
  *
  */
 function* get99BottlesOfBeer() {
-    for (let i = 99; i >= 0; --i) {
-        if (i === 1) yield `1 bottle of beer on the wall, 1 bottle of beer.`;
-        else if (i === 0) yield `No more bottles of beer on the wall, no more bottles of beer.`;
-        else yield `${i} bottles of beer on the wall, ${i} bottles of beer.`;
-        if (i === 2) yield `Take one down and pass it around, 1 bottle of beer on the wall.`;
-        else if (i === 1) yield `Take one down and pass it around, no more bottles of beer on the wall.`;
-        else if (i === 0) yield `Go to the store and buy some more, 99 bottles of beer on the wall.`;
-        else yield `Take one down and pass it around, ${i - 1} bottles of beer on the wall.`;
+    for (let i = 99; i > 2; --i ) {
+        yield(`${i} bottles of beer on the wall, ${i} bottles of beer.`);
+        yield(`Take one down and pass it around, ${i - 1} bottles of beer on the wall.`);
     }
+    yield('2 bottles of beer on the wall, 2 bottles of beer.');
+    yield('Take one down and pass it around, 1 bottle of beer on the wall.');
+    yield('1 bottle of beer on the wall, 1 bottle of beer.');
+    yield('Take one down and pass it around, no more bottles of beer on the wall.');
+    yield('No more bottles of beer on the wall, no more bottles of beer.');
+    yield('Go to the store and buy some more, 99 bottles of beer on the wall.');
 }
 
 
