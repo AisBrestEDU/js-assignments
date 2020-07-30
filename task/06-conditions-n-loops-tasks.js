49a917cd9a1f4e6cad8e773b31d7a67818e797ce
Gaziz666 <61174473+Gaziz666@users.noreply.github.com>
2020-07-30 14:28:02 +0300

    Update 06-conditions-n-loops-tasks.js

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -98,7 +98,9 @@ function getSumBetweenNumbers(n1, n2) {
 function isTriangle(a,b,c) {
     if ( a + b > c && a + c > b && b + c > a){
         return true;
-    } else return false
+    } else {
+        return false
+    }
 }
 
 
