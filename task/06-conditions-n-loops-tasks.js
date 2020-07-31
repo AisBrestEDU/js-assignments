a393ad019415f489dca87d14c944093bad250300
Roman <romayeah@yandex.ru>
2020-08-01 01:48:54 +0300

    refactor: refactor code

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -103,17 +103,7 @@ function getSumBetweenNumbers(n1, n2) {
  *   10,10,10 =>  true
  */
 function isTriangle(a,b,c) {
-    if ((a + b <= c) || (a + c <= b) || (b + c <= a)) {
-        return false;
-      } 
-    else {
-        if ((a <= 0) || (b <= 0) || (c <= 0)) {
-            return false;
-        } 
-        else {
-            return true;
-        } 
-    } 
+    return ((a + b <= c) || (a + c <= b) || (b + c <= a)) ? false : ((a <= 0) || (b <= 0) || (c <= 0)) ? false : true;
 }
 
 
