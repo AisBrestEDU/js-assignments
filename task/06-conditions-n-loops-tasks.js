b6d650f6f08241541f634770c4967f8be1094e34
Nastya Kosukha <47366500+AnastasiyaKos@users.noreply.github.com>
2020-08-02 19:03:42 +0300

    Update 06-conditions-n-loops-tasks.js

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -33,7 +33,7 @@ function getFizzBuzz(num) {
     if (num % 3 === 0 && num % 5 === 0) return 'FizzBuzz';
     else if (num % 3 === 0) return 'Fizz';
     else if (num % 5 === 0) return 'Buzz';
-    else return num;
+    return num;
 }
 
 
