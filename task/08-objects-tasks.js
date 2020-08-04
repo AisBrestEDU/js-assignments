569436cc96e396d7b7721bc021000484e8508b1b
Mikhail Pyshnik <misha.pyshnik@gmail.com>
2020-08-04 20:21:41 +0300

    Fixed comments

--- task/08-objects-tasks.js
+++ task/08-objects-tasks.js
@@ -23,9 +23,9 @@
  *    console.log(r.getArea());   // => 200
  */
 function Rectangle(width, height) {
-    this.width=width;
-    this.height=height;
-    Rectangle.prototype.getArea =  function() {
+    this.width = width;
+    this.height = height;
+    Rectangle.prototype.getArea = function() {
         return this.width * this.height;
     }
 }
