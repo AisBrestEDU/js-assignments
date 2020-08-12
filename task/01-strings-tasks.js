0f6943823e6f9f747ef2e888b2495ae938cf358a
StYaroslav <yaroslavstenko@gmail.com>
2020-08-12 15:32:16 +0300

    added tasks #01-04

--- task/01-strings-tasks.js
+++ task/01-strings-tasks.js
@@ -22,7 +22,7 @@
  *   '',  'bb'  => 'bb'
  */
 function concatenateStrings(value1, value2) {
-    throw new Error('Not implemented');
+    return value1 + value2;
 }
 
 
@@ -38,7 +38,7 @@ function concatenateStrings(value1, value2) {
  *   ''      => 0
  */
 function getStringLength(value) {
-    throw new Error('Not implemented');
+    return value.length;
 }
 
 /**
@@ -55,7 +55,7 @@ function getStringLength(value) {
  *   'Chuck','Norris'  => 'Hello, Chuck Norris!'
  */
 function getStringFromTemplate(firstName, lastName) {
-    throw new Error('Not implemented');
+    return `Hello, ${firstName + ' ' + lastName + '!'}`;
 }
 
 /**
@@ -69,7 +69,7 @@ function getStringFromTemplate(firstName, lastName) {
  *   'Hello, Chuck Norris!' => 'Chuck Norris'
  */
 function extractNameFromTemplate(value) {
-    throw new Error('Not implemented');
+    return value.substring(value.lastIndexOf(",") + 1, value.lastIndexOf("!")).trim();
 }
 
 
@@ -84,7 +84,7 @@ function extractNameFromTemplate(value) {
  *   'cat'       => 'c'
  */
 function getFirstChar(value) {
-    throw new Error('Not implemented');
+    return value[0];
 }
 
 /**
@@ -99,7 +99,7 @@ function getFirstChar(value) {
  *   '\tHello, World! ' => 'Hello, World!'
  */
 function removeLeadingAndTrailingWhitespaces(value) {
-    throw new Error('Not implemented');
+    return value.trim();
 }
 
 /**
@@ -114,7 +114,7 @@ function removeLeadingAndTrailingWhitespaces(value) {
  *   'cat', 3 => 'catcatcat'
  */
 function repeatString(value, count) {
-    throw new Error('Not implemented');
+    return value.repeat(parseInt(count));
 }
 
 /**
@@ -130,7 +130,7 @@ function repeatString(value, count) {
  *   'ABABAB','BA' => 'ABAB'
  */
 function removeFirstOccurrences(str, value) {
-    throw new Error('Not implemented');
+    return str.replace(value, '');
 }
 
 /**
@@ -145,7 +145,7 @@ function removeFirstOccurrences(str, value) {
  *   '<a>' => 'a'
  */
 function unbracketTag(str) {
-    throw new Error('Not implemented');
+    return str.match(/[^<>]/g).join('');
 }
 
 
@@ -160,7 +160,7 @@ function unbracketTag(str) {
  *  'abcdefghijklmnopqrstuvwxyz' => 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
  */
 function convertToUpperCase(str) {
-    throw new Error('Not implemented');
+    return str.toUpperCase();
 }
 
 /**
@@ -174,7 +174,7 @@ function convertToUpperCase(str) {
  *   'info@gmail.com' => ['info@gmail.com']
  */
 function extractEmails(str) {
-    throw new Error('Not implemented');
+    return str.split(';');
 }
 
 /**
@@ -201,7 +201,27 @@ function extractEmails(str) {
  *
  */
 function getRectangleString(width, height) {
-    throw new Error('Not implemented');
+    let cornerLeftUp = '┌';
+    let cornerLeftDown = '└';
+    let cornerRightUp = '┐';
+    let cornerRightDown = '┘'
+    let horizontalLine = '─';
+    let verticalLine = '│';
+    let innerSymbol = ' ';
+    let resultStr = '';
+    for (let i = 1; i <= height; i++) {
+        switch (i) {
+            case 1:
+                resultStr += cornerLeftUp + horizontalLine.repeat(width - 2) + cornerRightUp + '\n';
+                break;
+            case height:
+                resultStr += cornerLeftDown + horizontalLine.repeat(width - 2) + cornerRightDown + '\n';
+                break;
+            default:
+                resultStr += verticalLine + innerSymbol.repeat(width - 2) + verticalLine + '\n';
+        }
+    }
+    return resultStr;
 }
 
 
@@ -221,7 +241,13 @@ function getRectangleString(width, height) {
  *
  */
 function encodeToRot13(str) {
-    throw new Error('Not implemented');
+    let resultStr = '';
+    for (let i = 0; i < str.length; i++){
+        if (str[i].match(/[A-Ma-m]/)) resultStr += String.fromCharCode(str.charCodeAt(i) + 13);
+        else if(str[i].match(/[N-Zn-z]/)) resultStr += String.fromCharCode(str.charCodeAt(i) - 13);
+        else resultStr += str[i];
+    }
+    return resultStr;
 }
 
 /**
@@ -238,7 +264,7 @@ function encodeToRot13(str) {
  *   isString(new String('test')) => true
  */
 function isString(value) {
-    throw new Error('Not implemented');
+    return typeof value === "string" || value instanceof String;
 }
 
 
@@ -267,7 +293,13 @@ function isString(value) {
  *   'K♠' => 51
  */
 function getCardId(value) {
-    throw new Error('Not implemented');
+    let cardDeck = [
+        'A♣','2♣','3♣','4♣','5♣','6♣','7♣','8♣','9♣','10♣','J♣','Q♣','K♣',
+        'A♦','2♦','3♦','4♦','5♦','6♦','7♦','8♦','9♦','10♦','J♦','Q♦','K♦',
+        'A♥','2♥','3♥','4♥','5♥','6♥','7♥','8♥','9♥','10♥','J♥','Q♥','K♥',
+        'A♠','2♠','3♠','4♠','5♠','6♠','7♠','8♠','9♠','10♠','J♠','Q♠','K♠'
+    ]
+    return cardDeck.indexOf(value);
 }
 
 
--- task/02-numbers-tasks.js
+++ task/02-numbers-tasks.js
@@ -22,7 +22,7 @@
  *   5, 5  => 25
  */
 function getRectangleArea(width, height) {
-    throw new Error('Not implemented');
+    return width * height;
 }
 
 
@@ -38,7 +38,7 @@ function getRectangleArea(width, height) {
  *   0    => 0
  */
 function getCicleCircumference(radius) {
-    throw new Error('Not implemented');
+    return 2 * Math.PI * radius;
 }
 
 /**
@@ -54,7 +54,7 @@ function getCicleCircumference(radius) {
  *  -3, 3  => 0
  */
 function getAverage(value1, value2) {
-    throw new Error('Not implemented');
+    return Math.ceil(value1 / 2 + value2 / 2);
 }
 
 /**
@@ -73,7 +73,7 @@ function getAverage(value1, value2) {
  *   (-5,0) (10,-10) => 18.027756377319946
  */
 function getDistanceBetweenPoints(x1, y1, x2, y2) {
-    throw new Error('Not implemented');
+    return Math.sqrt(Math.pow((x2 - x1), 2) + Math.pow((y2 - y1), 2));
 }
 
 /**
@@ -89,7 +89,8 @@ function getDistanceBetweenPoints(x1, y1, x2, y2) {
  *   5*x = 0         => 0
  */
 function getLinearEquationRoot(a, b) {
-    throw new Error('Not implemented');
+    let result = -b / a;
+    return result === 0 ? 0 : result;
 }
 
 
@@ -111,7 +112,10 @@ function getLinearEquationRoot(a, b) {
  *   (0,1) (1,2)     => 0
  */
 function getAngleBetweenVectors(x1, y1, x2, y2) {
-    throw new Error('Not implemented');
+    let vectorsDotProduct = (x1 * x2) + (y1 * y2);
+    let firstVectorMagnitude = Math.sqrt((Math.pow(x1, 2)) + Math.pow(y1, 2));
+    let secondVectorMagnitude = Math.sqrt((Math.pow(x2, 2)) + Math.pow(y2, 2));
+    return Math.acos(vectorsDotProduct / (firstVectorMagnitude * secondVectorMagnitude));
 }
 
 /**
@@ -127,7 +131,11 @@ function getAngleBetweenVectors(x1, y1, x2, y2) {
  *     0     => 0
  */
 function getLastDigit(value) {
-    throw new Error('Not implemented');
+    let result = 0;
+    for(let i = 0; i < value.toString().length; i++){
+        result = value % 10;
+    }
+    return result;
 }
 
 
@@ -143,7 +151,7 @@ function getLastDigit(value) {
  * '-525.5'     => -525.5
  */
 function parseNumberFromString(value) {
-    throw new Error('Not implemented');
+    return Number(value);
 }
 
 /**
@@ -160,7 +168,7 @@ function parseNumberFromString(value) {
  *   1,2,3   => 3.741657386773941
  */
 function getParallelipidedDiagonal(a,b,c) {
-    throw new Error('Not implemented');
+    return Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2) + Math.pow(c, 2));
 }
 
 /**
@@ -181,7 +189,9 @@ function getParallelipidedDiagonal(a,b,c) {
  *   1678, 3  => 2000
  */
 function roundToPowerOfTen(num, pow) {
-    throw new Error('Not implemented');
+    let smallerMul = (num - num % Math.pow(10, pow)) / Math.pow(10, pow) * Math.pow(10, pow);
+    let biggerMul = smallerMul + Math.pow(10, pow);
+    return num - smallerMul > biggerMul - num ? biggerMul : smallerMul;
 }
 
 /**
@@ -202,7 +212,10 @@ function roundToPowerOfTen(num, pow) {
  *   17 => true
  */
 function isPrime(n) {
-    throw new Error('Not implemented');
+    for (let i = 2; i <= Math.sqrt(n); i++)
+        if (n % i === 0)
+            return false;
+    return true;
 }
 
 /**
@@ -221,7 +234,10 @@ function isPrime(n) {
  *   toNumber(new Number(42), 0) => 42
  */
 function toNumber(value, def) {
-    throw new Error('Not implemented');
+    if (isNaN(Number(value))) {
+        return def;
+    }
+    return Number(value);
 }
 
 module.exports = {
