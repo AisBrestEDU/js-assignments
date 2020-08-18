0b1abed4875549d0bbd61177b334b35cbe7ebe5d
Irina <master@192.168.100.7>
2020-08-18 15:45:26 +0300

    fix Rectangle function

--- task/08-objects-tasks.js
+++ task/08-objects-tasks.js
@@ -25,8 +25,8 @@
 function Rectangle(width, height) {
         this.width = width
         this.height = height
-        Rectangle.prototype.getArea = () => this.width * this.height
 }
+Rectangle.prototype.getArea = () => this.width * this.height
 
 
 /**
