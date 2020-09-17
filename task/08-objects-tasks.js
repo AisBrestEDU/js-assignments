77f15c30a4de39d5f45d82a8a94e17c5743d2188
Nikita <mironov.nik88@gmail.com>
2020-09-17 12:13:36 +0300

    task_8

--- task/08-objects-tasks.js
+++ task/08-objects-tasks.js
@@ -24,10 +24,10 @@
  */
 function Rectangle(width, height) {
     this.width = width;
-	this.height = height;
+    this.height = height;
     Object.defineProperty(this, 'getArea', {
-		value: function()  {return this.width * this.height;}
-	});
+        value: function () { return this.width * this.height; }
+    });
 }
 
 
@@ -112,31 +112,31 @@ function fromJSON(proto, json) {
 
 const cssSelectorBuilder = {
 
-    element: function(value) {
+    element: function (value) {
         throw new Error('Not implemented');
     },
 
-    id: function(value) {
+    id: function (value) {
         throw new Error('Not implemented');
     },
 
-    class: function(value) {
+    class: function (value) {
         throw new Error('Not implemented');
     },
 
-    attr: function(value) {
+    attr: function (value) {
         throw new Error('Not implemented');
     },
 
-    pseudoClass: function(value) {
+    pseudoClass: function (value) {
         throw new Error('Not implemented');
     },
 
-    pseudoElement: function(value) {
+    pseudoElement: function (value) {
         throw new Error('Not implemented');
     },
 
-    combine: function(selector1, combinator, selector2) {
+    combine: function (selector1, combinator, selector2) {
         throw new Error('Not implemented');
     },
 };
